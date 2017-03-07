5793a45c0452e3664eff3953b7bda5920f63401d
Ian Cottrell <iancottrell@google.com>
2017-03-07 12:07:43 +0000

    kill the integration build tag

--- test/integration/replay/gles/direct_call_test.go
+++ test/integration/replay/gles/direct_call_test.go
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build integration
-
 package gles
 
 import "github.com/google/gapid/gapis/atom"
