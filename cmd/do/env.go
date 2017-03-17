c9e3ec5474bd667255b5d42a43407b813acd0e2b
Ben Clayton <headlessclayton@gmail.com>
2017-03-17 16:05:59 +0000

    Start with a clean PATH.
    Inheriting from the shell's environment leads to exciting
    inconsistent behavior.

---
+++ cmd/do/env.go
@@ -0,0 +1,54 @@
+// Copyright (C) 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"runtime"
+
+	"github.com/google/gapid/core/os/file"
+	"github.com/google/gapid/core/os/shell"
+)
+
+func env(cfg Config) *shell.Env {
+	path := []string{}
+
+	if runtime.GOOS == "windows" {
+		if !cfg.MSYS2Path.IsEmpty() {
+			path = append(path,
+				cfg.MSYS2Path.Join("usr/bin").System(),     // Required for sh.exe and other unixy tools.
+				cfg.MSYS2Path.Join("mingw64/bin").System(), // Required to pick up DLLs
+			)
+		}
+
+		// Add windows and system32 to path
+		cmd, err := file.FindExecutable("cmd.exe")
+		if err != nil {
+			panic("Couldn't find cmd.exe on PATH")
+		}
+		system32 := cmd.Parent()
+		windows := system32.Parent()
+		path = append(path, system32.System(), windows.System())
+	}
+
+	path = append(path,
+		cfg.bin().System(),
+		cfg.JavaHome.Join("bin").System(),
+	)
+
+	env := shell.CloneEnv()
+	env.Unset("PATH")
+	env.AddPathEnd("PATH", path...)
+	return env
+}
