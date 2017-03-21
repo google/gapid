170092f92cffe74abb309dfc55f4e47d86cebc1b
Ben Clayton <headlessclayton@gmail.com>
2017-03-21 19:43:01 +0000

    Reduce some of the GAPIR spam to DEBUG level.

--- gapir/cc/server_listener.cpp
+++ gapir/cc/server_listener.cpp
@@ -46,14 +46,14 @@ ServerListener::ServerListener(std::unique_ptr<core::Connection> conn, uint64_t 
 
 std::unique_ptr<ServerConnection> ServerListener::acceptConnection(int idleTimeoutMs, const char* authToken) {
     while (true) {
-        GAPID_INFO("Waiting for new connection...");
+        GAPID_DEBUG("Waiting for new connection...");
         std::unique_ptr<core::Connection> client = mConn->accept(idleTimeoutMs);
         if (client == nullptr) {
             return nullptr;
         }
 
         if (authToken != nullptr) {
-            GAPID_INFO("Checking auth-token...");
+            GAPID_DEBUG("Checking auth-token...");
             char header[sizeof(kAuthTokenHeader)];
             if (client->recv(&header, sizeof(header)) != sizeof(header)) {
                 GAPID_WARNING("Failed to read auth-token header");
