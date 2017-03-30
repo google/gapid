719f0e90c87ba41d269cbb34043731738324847d
Ben Clayton <bclayton@google.com>
2017-03-30 16:47:26 +0100

    gapic: Add file copyright and a few comments.
    Change-Id: I93e4bb2f7d6c649d8f622566823af2596dc679e3

--- gapic/src/main/com/google/gapid/views/TreeHelper.java
+++ gapic/src/main/com/google/gapid/views/TreeHelper.java
@@ -1,12 +1,30 @@
+/*
+ * Copyright (C) 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.gapid.views;
 
 import org.eclipse.swt.widgets.Tree;
 import org.eclipse.swt.widgets.TreeItem;
 
 /**
- * Created by bclayton on 3/30/17.
+ * A static helper class for dealing with {@link Tree}s and {@link TreeItem}s.
  */
 public class TreeHelper {
+  /**
+   * @return the i'th child {@link TreeItem} of the {@link Tree} or {@link TreeItem}.
+   */
   public static TreeItem getItem(Object treeOrTreeItem, int index) {
     if (treeOrTreeItem instanceof Tree) {
       return ((Tree)treeOrTreeItem).getItem(index);
@@ -14,6 +32,9 @@ public class TreeHelper {
     return ((TreeItem)treeOrTreeItem).getItem(index);
   }
 
+  /**
+   * @return the index of the child {@link TreeItem} of the {@link Tree} or {@link TreeItem}.
+   */
   public static int indexOf(Object treeOrTreeItem, TreeItem child) {
     if (treeOrTreeItem instanceof Tree) {
       return ((Tree)treeOrTreeItem).indexOf(child);
@@ -21,6 +42,9 @@ public class TreeHelper {
     return ((TreeItem)treeOrTreeItem).indexOf(child);
   }
 
+  /**
+   * @return the number of child {@link TreeItem}s of the {@link Tree} or {@link TreeItem}.
+   */
   public static int itemCount(Object treeOrTreeItem) {
     if (treeOrTreeItem instanceof Tree) {
       return ((Tree)treeOrTreeItem).getItemCount();
@@ -28,8 +52,11 @@ public class TreeHelper {
     return ((TreeItem)treeOrTreeItem).getItemCount();
   }
 
+  /**
+   * @return the parent {@link Tree} or {@link TreeItem} of the {@link TreeItem}.
+   */
   public static Object getParent(Object treeOrTreeItem) {
-    if (treeOrTreeItem instanceof Tree) {
+    if (!(treeOrTreeItem instanceof TreeItem)) {
       return null;
     }
     TreeItem item = (TreeItem)treeOrTreeItem;
--- gapic/src/main/com/google/gapid/views/VirtualTreeViewer.java
+++ gapic/src/main/com/google/gapid/views/VirtualTreeViewer.java
@@ -1,8 +1,22 @@
+/*
+ * Copyright (C) 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.gapid.views;
 
 import org.eclipse.jface.viewers.IBaseLabelProvider;
 import org.eclipse.jface.viewers.IContentProvider;
-import org.eclipse.jface.viewers.ILabelProvider;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Tree;
@@ -12,20 +26,30 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.logging.Logger;
 
 /**
- * A virtual tree content provider.
+ * A {@link TreeViewer} that notifies the bound {@link IContentProvider} and
+ * {@link IBaseLabelProvider} of time visibility changes if they also implement
+ * the {@link VisibilityListener} interface.
  */
 public class VirtualTreeViewer extends TreeViewer {
-  private static final Logger LOG = Logger.getLogger(VirtualTreeViewer.class.getName());
-
-  private Set<TreeItem> visible = new HashSet<>();
-
+  /**
+   * The interface the {@link IContentProvider} and {@link IBaseLabelProvider} can
+   * implement to be notified about {@link TreeItem}s being made visible and hidden
+   * usually by scrolling.
+   */
   public interface VisibilityListener {
+    /**
+     * Called when the {@link TreeItem} is made visible.
+     */
     void OnShow(TreeItem item);
+    /**
+     * Called when the {@link TreeItem} is made invisible.
+     */
     void OnHide(TreeItem item);
   }
+
+  private Set<TreeItem> visible = new HashSet<>();
 
   public VirtualTreeViewer(Tree tree) {
     super(tree);
