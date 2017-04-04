google/gapid

commit f7d41ba29ee0033d87d629948c5418b549bf439c

Resolve all resources for a single frame at a time.
This means for getting texture thumbnails for example,
we run a single replay rather than one replay for each image.

gapis/resolve/resource_data.go
@@ -18,11 +18,68 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/google/gapid/core/data/id"
 	"github.com/google/gapid/gapis/capture"
 	"github.com/google/gapid/gapis/database"
 	"github.com/google/gapid/gapis/gfxapi"
 	"github.com/google/gapid/gapis/service/path"
 )
+
+// ResolvedResources contains all of the resolved resources for a
+// particular point in the trace.
+type ResolvedResources struct {
+	resourceMap  gfxapi.ResourceMap
+	resources    map[id.ID]gfxapi.Resource
+	resourceData map[id.ID]interface{}
+}
+
+// Resolve builds a ResolvedResources object for all of the resources
+// at the path r.After
+func (r *AllResourceDataResolvable) Resolve(ctx context.Context) (interface{}, error) {
+	ctx = capture.Put(ctx, r.After.Commands.Capture)
+	resources, err := buildResources(ctx, r.After)
+
+	if err != nil {
+		return nil, err
+	}
+	return resources, nil
+}
+
+func buildResources(ctx context.Context, p *path.Command) (*ResolvedResources, error) {
+	list, err := NCommands(ctx, p.Commands, p.Index+1)
+	if err != nil {
+		return nil, err
+	}
+	state := capture.NewState(ctx)
+	var currentAtomIndex uint64
+	var currentAtomResourceCount int
+	idMap := gfxapi.ResourceMap{}
+
+	resources := make(map[id.ID]gfxapi.Resource)
+
+	state.OnResourceCreated = func(r gfxapi.Resource) {
+		currentAtomResourceCount++
+		i := genResourceID(currentAtomIndex, currentAtomResourceCount)
+		idMap[r] = i
+		resources[i] = r
+	}
+
+	for i, a := range list.Atoms[:p.Index+1] {
+		currentAtomResourceCount = 0
+		currentAtomIndex = uint64(i)
+		a.Mutate(ctx, state, nil /* no builder, just mutate */)
+	}
+
+	resourceData := make(map[id.ID]interface{})
+	for k, v := range resources {
+		res, err := v.ResourceData(ctx, state)
+		if err != nil {
+			return nil, fmt.Errorf("Could not get data for resource %v", k)
+		}
+		resourceData[k] = res
+	}
+	return &ResolvedResources{idMap, resources, resourceData}, nil
+}
 
 // ResourceData resolves the data of the specified resource at the specified
 // point in the capture.
@@ -36,42 +93,18 @@ func ResourceData(ctx context.Context, p *path.ResourceData) (interface{}, error
 
 // Resolve implements the database.Resolver interface.
 func (r *ResourceDataResolvable) Resolve(ctx context.Context) (interface{}, error) {
-	ctx = capture.Put(ctx, r.Path.After.Commands.Capture)
-
-	state := capture.NewState(ctx)
-	IDMap := gfxapi.ResourceMap{}
-	// TODO: Persist state in getResourceMeta and reuse it here.
-	resource, err := buildResource(ctx, state, r.Path, IDMap)
+	resources, err := database.Build(ctx, &AllResourceDataResolvable{r.Path.After})
 	if err != nil {
 		return nil, err
 	}
-	return resource.ResourceData(ctx, state, IDMap)
-}
+	res, ok := resources.(*ResolvedResources)
+	if !ok {
+		return nil, fmt.Errorf("Cannot resolve resources at command: %v", r.Path.After)
+	}
+	id := r.Path.Id.ID()
+	if val, ok := res.resourceData[id]; ok {
+		return val, nil
+	}
 
-func buildResource(ctx context.Context, state *gfxapi.State, p *path.ResourceData, IDMap gfxapi.ResourceMap) (gfxapi.Resource, error) {
-	list, err := NCommands(ctx, p.After.Commands, p.After.Index+1)
-	if err != nil {
-		return nil, err
-	}
-	var currentAtomIndex uint64
-	var resource gfxapi.Resource
-	var currentAtomResourceCount int
-	id := p.Id.ID()
-	state.OnResourceCreated = func(r gfxapi.Resource) {
-		currentAtomResourceCount++
-		i := genResourceID(currentAtomIndex, currentAtomResourceCount)
-		IDMap[r] = i
-		if i == id {
-			resource = r
-		}
-	}
-	for i, a := range list.Atoms[:p.After.Index+1] {
-		currentAtomResourceCount = 0
-		currentAtomIndex = uint64(i)
-		a.Mutate(ctx, state, nil /* no builder, just mutate */)
-	}
-	if resource != nil {
-		return resource, nil
-	}
-	return nil, fmt.Errorf("Resource with id %v not found", p.Id.ID())
+	return nil, fmt.Errorf("Cannot find resource with id: %v", id)
 }

gapis/gfxapi/gles/resources.go
@@ -63,7 +63,7 @@ func (t *Texture) ResourceType() gfxapi.ResourceType {
 }
 
 // ResourceData returns the resource data given the current state.
-func (t *Texture) ResourceData(ctx context.Context, s *gfxapi.State, resources gfxapi.ResourceMap) (interface{}, error) {
+func (t *Texture) ResourceData(ctx context.Context, s *gfxapi.State) (interface{}, error) {
 	ctx = log.Enter(ctx, "Texture.Resource()")
 	switch t.Kind {
 	case GLenum_GL_TEXTURE_2D:
@@ -143,7 +143,7 @@ func (s *Shader) ResourceType() gfxapi.ResourceType {
 }
 
 // ResourceData returns the resource data given the current state.
-func (s *Shader) ResourceData(ctx context.Context, t *gfxapi.State, resources gfxapi.ResourceMap) (interface{}, error) {
+func (s *Shader) ResourceData(ctx context.Context, t *gfxapi.State) (interface{}, error) {
 	ctx = log.Enter(ctx, "Shader.Resource()")
 	var ty gfxapi.ShaderType
 	switch s.ShaderType {
@@ -241,7 +241,7 @@ func (p *Program) ResourceType() gfxapi.ResourceType {
 }
 
 // ResourceData returns the resource data given the current state.
-func (p *Program) ResourceData(ctx context.Context, s *gfxapi.State, resources gfxapi.ResourceMap) (interface{}, error) {
+func (p *Program) ResourceData(ctx context.Context, s *gfxapi.State) (interface{}, error) {
 	ctx = log.Enter(ctx, "Program.Resource()")
 	state := GetState(s)
 	context := state.Contexts.Get(state.CurrentThread)

gapis/gfxapi/resource.go
@@ -40,7 +40,7 @@ type Resource interface {
 	ResourceType() ResourceType
 
 	// ResourceData returns the resource data given the current state.
-	ResourceData(ctx context.Context, s *State, resources ResourceMap) (interface{}, error)
+	ResourceData(ctx context.Context, s *State) (interface{}, error)
 
 	// SetResourceData sets resource data in a new capture.
 	SetResourceData(ctx context.Context, at *path.Command, data interface{}, resources ResourceMap, edits ReplaceCallback) error

gapis/gfxapi/vulkan/resources.go
@@ -176,7 +176,7 @@ func setCubemapFace(img *image.Info2D, cubeMap *gfxapi.CubemapLevel, layerIndex 
 }
 
 // ResourceData returns the resource data given the current state.
-func (t *ImageObject) ResourceData(ctx context.Context, s *gfxapi.State, resources gfxapi.ResourceMap) (interface{}, error) {
+func (t *ImageObject) ResourceData(ctx context.Context, s *gfxapi.State) (interface{}, error) {
 	ctx = log.Enter(ctx, "ImageObject.Resource()")
 
 	vkFmt := t.Info.Format
@@ -256,7 +256,7 @@ func (s *ShaderModuleObject) ResourceType() gfxapi.ResourceType {
 }
 
 // ResourceData returns the resource data given the current state.
-func (s *ShaderModuleObject) ResourceData(ctx context.Context, t *gfxapi.State, resources gfxapi.ResourceMap) (interface{}, error) {
+func (s *ShaderModuleObject) ResourceData(ctx context.Context, t *gfxapi.State) (interface{}, error) {
 	ctx = log.Enter(ctx, "Shader.ResourceData()")
 	words := s.Words.Read(ctx, nil, t, nil)
 	source := shadertools.DisassembleSpirvBinary(words)

gapis/resolve/resolvables.proto
@@ -77,6 +77,10 @@ message ResourceDataResolvable {
 	path.ResourceData path = 1;
 }
 
+message AllResourceDataResolvable {
+	path.Command after = 1;
+}
+
 message ResourceMetaResolvable {
 	path.ID id = 1;
 	path.Command after = 2;

gapis/resolve/resource_meta.go
@@ -16,8 +16,8 @@ package resolve
 
 import (
 	"context"
+	"fmt"
 
-	"github.com/google/gapid/gapis/capture"
 	"github.com/google/gapid/gapis/database"
 	"github.com/google/gapid/gapis/gfxapi"
 	"github.com/google/gapid/gapis/service/path"
@@ -34,16 +34,22 @@ func ResourceMeta(ctx context.Context, id *path.ID, after *path.Command) (*gfxap
 
 // Resolve implements the database.Resolver interface.
 func (r *ResourceMetaResolvable) Resolve(ctx context.Context) (interface{}, error) {
-	ctx = capture.Put(ctx, r.After.Commands.Capture)
-	state := capture.NewState(ctx)
-	result := &gfxapi.ResourceMeta{
-		IDMap: gfxapi.ResourceMap{},
-	}
-	p := &path.ResourceData{Id: r.Id, After: r.After}
-	resource, err := buildResource(ctx, state, p, result.IDMap)
+	resources, err := database.Build(ctx, &AllResourceDataResolvable{r.After})
 	if err != nil {
 		return nil, err
 	}
-	result.Resource = resource
+	res, ok := resources.(*ResolvedResources)
+	if !ok {
+		return nil, fmt.Errorf("Cannot resolve resources at command: %v", r.After)
+	}
+	id := r.Id.ID()
+	val, ok := res.resources[id]
+	if !ok {
+		return nil, fmt.Errorf("Could not find resource %v", id)
+	}
+	result := &gfxapi.ResourceMeta{
+		IDMap:    res.resourceMap,
+		Resource: val,
+	}
 	return result, nil
 }

commit 84284b79ceb1e65f88a8866dbf8d4020ab9821da

Fix resource data bug introduced in commit f7d41ba.
Don't fail all resources if a resource fails to load, instead
remember the error and return it if that specific resource is
requested.

gapis/resolve/resource_data.go
@@ -74,9 +74,10 @@ func buildResources(ctx context.Context, p *path.Command) (*ResolvedResources, e
 	for k, v := range resources {
 		res, err := v.ResourceData(ctx, state)
 		if err != nil {
-			return nil, fmt.Errorf("Could not get data for resource %v", k)
+			resourceData[k] = err;
+		} else {
+			resourceData[k] = res
 		}
-		resourceData[k] = res
 	}
 	return &ResolvedResources{idMap, resources, resourceData}, nil
 }
@@ -103,6 +104,9 @@ func (r *ResourceDataResolvable) Resolve(ctx context.Context) (interface{}, erro
 	}
 	id := r.Path.Id.ID()
 	if val, ok := res.resourceData[id]; ok {
+		if err, isErr := val.(error); isErr {
+			return nil, err
+		}
 		return val, nil
 	}
 