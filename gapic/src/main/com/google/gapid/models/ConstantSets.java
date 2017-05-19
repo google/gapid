76cd4396c7548d951d75884377f9829d11abeab3
Pascal Muetschard <pmuetschard@google.com>
2017-05-19 16:10:13 -0700

    Atom editing UI for new proto world.

--- gapic/src/main/com/google/gapid/models/ConstantSets.java
+++ gapic/src/main/com/google/gapid/models/ConstantSets.java
@@ -19,10 +19,12 @@ import com.google.common.collect.Lists;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.gapid.proto.service.Service;
+import com.google.gapid.proto.service.box.Box;
 import com.google.gapid.proto.service.path.Path;
 import com.google.gapid.server.Client;
 import com.google.gapid.util.FutureCache;
 import com.google.gapid.util.Paths;
+import com.google.gapid.util.Pods;
 
 import java.util.List;
 
@@ -58,5 +60,18 @@ public class ConstantSets {
 
   public Service.ConstantSet getConstants(Path.ConstantSet path) {
     return cache.getIfPresent(path);
+  }
+
+  public static Service.Constant find(Service.ConstantSet constants, Box.Value value) {
+    if (value.getValCase() != Box.Value.ValCase.POD || !Pods.mayBeConstant(value.getPod())) {
+      return Service.Constant.getDefaultInstance();
+    }
+    long numValue = Pods.getConstant(value.getPod());
+    for (Service.Constant constant : constants.getConstantsList()) {
+      if (constant.getValue() == numValue) {
+        return constant;
+      }
+    }
+    return Service.Constant.getDefaultInstance();
   }
 }
