google/gapid

commit b03855a46c10454a381cf5c3afaaee8236ecc086

Clean up resource.proto
Implement the changes as proposed by #237.

Fixes: #237

gapis/gfxapi/gles/resources.go
@@ -52,21 +52,11 @@ func (t *Texture) Order() uint64 {
 
 // ResourceType returns the type of this resource.
 func (t *Texture) ResourceType(ctx context.Context) gfxapi.ResourceType {
-	switch t.Kind {
-	case GLenum_GL_NONE:
-		return gfxapi.ResourceType_UninitializedTextureResource
-	case GLenum_GL_TEXTURE_2D:
-		return gfxapi.ResourceType_Texture2DResource
-	case GLenum_GL_TEXTURE_CUBE_MAP:
-		return gfxapi.ResourceType_CubemapResource
-	default:
-		log.E(ctx, "Unknown texture resource type: %v", t.Kind)
-		return gfxapi.ResourceType_UnknownResource
-	}
+	return gfxapi.ResourceType_TextureResource
 }
 
 // ResourceData returns the resource data given the current state.
-func (t *Texture) ResourceData(ctx context.Context, s *gfxapi.State) (interface{}, error) {
+func (t *Texture) ResourceData(ctx context.Context, s *gfxapi.State) (*gfxapi.ResourceData, error) {
 	ctx = log.Enter(ctx, "Texture.ResourceData()")
 	switch t.Kind {
 	case GLenum_GL_TEXTURE_2D:
@@ -84,7 +74,7 @@ func (t *Texture) ResourceData(ctx context.Context, s *gfxapi.State) (interface{
 				Data:   image.NewID(img.Data.ResourceID(ctx, s)),
 			}
 		}
-		return &gfxapi.Texture2D{Levels: levels}, nil
+		return gfxapi.NewResourceData(gfxapi.NewTexture(&gfxapi.Texture2D{Levels: levels})), nil
 
 	case GLenum_GL_TEXTURE_CUBE_MAP:
 		levels := make([]*gfxapi.CubemapLevel, len(t.Levels))
@@ -117,7 +107,9 @@ func (t *Texture) ResourceData(ctx context.Context, s *gfxapi.State) (interface{
 				}
 			}
 		}
-		return &gfxapi.Cubemap{Levels: levels}, nil
+		return gfxapi.NewResourceData(gfxapi.NewTexture(&gfxapi.Texture_Cubemap{
+			Cubemap: &gfxapi.Cubemap{Levels: levels},
+		})), nil
 
 	default:
 		return nil, &service.ErrDataUnavailable{Reason: messages.ErrNoTextureData(t.ResourceHandle())}
@@ -125,7 +117,7 @@ func (t *Texture) ResourceData(ctx context.Context, s *gfxapi.State) (interface{
 }
 
 func (t *Texture) SetResourceData(ctx context.Context, at *path.Command,
-	data interface{}, resources gfxapi.ResourceMap, edits gfxapi.ReplaceCallback) error {
+	data *gfxapi.ResourceData, resources gfxapi.ResourceMap, edits gfxapi.ReplaceCallback) error {
 	return fmt.Errorf("SetResourceData is not supported for Texture")
 }
 
@@ -155,7 +147,7 @@ func (s *Shader) ResourceType(ctx context.Context) gfxapi.ResourceType {
 }
 
 // ResourceData returns the resource data given the current state.
-func (s *Shader) ResourceData(ctx context.Context, t *gfxapi.State) (interface{}, error) {
+func (s *Shader) ResourceData(ctx context.Context, t *gfxapi.State) (*gfxapi.ResourceData, error) {
 	ctx = log.Enter(ctx, "Shader.ResourceData()")
 	var ty gfxapi.ShaderType
 	switch s.ShaderType {
@@ -172,13 +164,14 @@ func (s *Shader) ResourceData(ctx context.Context, t *gfxapi.State) (interface{}
 	case GLenum_GL_COMPUTE_SHADER:
 		ty = gfxapi.ShaderType_Compute
 	}
-	return &gfxapi.Shader{Type: ty, Source: s.Source}, nil
+
+	return gfxapi.NewResourceData(&gfxapi.Shader{Type: ty, Source: s.Source}), nil
 }
 
 func (shader *Shader) SetResourceData(
 	ctx context.Context,
 	at *path.Command,
-	data interface{},
+	data *gfxapi.ResourceData,
 	resourceIDs gfxapi.ResourceMap,
 	edits gfxapi.ReplaceCallback) error {
 
@@ -219,9 +212,9 @@ func (shader *Shader) SetResourceData(
 	return fmt.Errorf("No atom to set data in")
 }
 
-func (a *GlShaderSource) Replace(ctx context.Context, data interface{}) gfxapi.ResourceAtom {
+func (a *GlShaderSource) Replace(ctx context.Context, data *gfxapi.ResourceData) gfxapi.ResourceAtom {
 	state := capture.NewState(ctx)
-	shader := data.(*gfxapi.Shader)
+	shader := data.GetShader()
 	source := shader.Source
 	src, _ := atom.AllocData(ctx, state, source)
 	srcLen, _ := atom.AllocData(ctx, state, GLint(len(source)))
@@ -258,7 +251,7 @@ func (p *Program) ResourceType(ctx context.Context) gfxapi.ResourceType {
 }
 
 // ResourceData returns the resource data given the current state.
-func (p *Program) ResourceData(ctx context.Context, s *gfxapi.State) (interface{}, error) {
+func (p *Program) ResourceData(ctx context.Context, s *gfxapi.State) (*gfxapi.ResourceData, error) {
 	ctx = log.Enter(ctx, "Program.ResourceData()")
 	state := GetState(s)
 
@@ -439,7 +432,7 @@ func (p *Program) ResourceData(ctx context.Context, s *gfxapi.State) (interface{
 		})
 	}
 
-	return &gfxapi.Program{Shaders: shaders, Uniforms: uniforms}, nil
+	return gfxapi.NewResourceData(&gfxapi.Program{Shaders: shaders, Uniforms: uniforms}), nil
 }
 
 func uniformValue(ctx context.Context, s *gfxapi.State, kind gfxapi.UniformType, data U8ˢ) interface{} {
@@ -482,6 +475,6 @@ func uniformValue(ctx context.Context, s *gfxapi.State, kind gfxapi.UniformType,
 }
 
 func (program *Program) SetResourceData(ctx context.Context, at *path.Command,
-	data interface{}, resources gfxapi.ResourceMap, edits gfxapi.ReplaceCallback) error {
+	data *gfxapi.ResourceData, resources gfxapi.ResourceMap, edits gfxapi.ReplaceCallback) error {
 	return fmt.Errorf("SetResourceData is not supported for Program")
 }

gapis/gfxapi/vulkan/resources.go
@@ -62,11 +62,7 @@ func (t *ImageObject) Order() uint64 {
 
 // ResourceType returns the type of this resource.
 func (t *ImageObject) ResourceType(ctx context.Context) gfxapi.ResourceType {
-	if uint32(t.Info.Flags)&uint32(VkImageCreateFlagBits_VK_IMAGE_CREATE_CUBE_COMPATIBLE_BIT) != 0 {
-		return gfxapi.ResourceType_CubemapResource
-	} else {
-		return gfxapi.ResourceType_Texture2DResource
-	}
+	return gfxapi.ResourceType_TextureResource
 }
 
 type unsupportedVulkanFormatError struct {
@@ -500,9 +496,8 @@ func setCubemapFace(img *image.Info2D, cubeMap *gfxapi.CubemapLevel, layerIndex 
 }
 
 // ResourceData returns the resource data given the current state.
-func (t *ImageObject) ResourceData(ctx context.Context, s *gfxapi.State) (interface{}, error) {
+func (t *ImageObject) ResourceData(ctx context.Context, s *gfxapi.State) (*gfxapi.ResourceData, error) {
 	ctx = log.Enter(ctx, "ImageObject.ResourceData()")
-
 	vkFmt := t.Info.Format
 	format, err := getImageFormatFromVulkanFormat(vkFmt)
 	if err != nil {
@@ -531,26 +526,28 @@ func (t *ImageObject) ResourceData(ctx context.Context, s *gfxapi.State) (interf
 					}
 				}
 			}
-			return &gfxapi.Cubemap{Levels: cubeMapLevels}, nil
-		} else {
-			levels := make([]*image.Info2D, len(t.Layers[0].Levels))
-			for i, level := range t.Layers[0].Levels {
-				levels[i] = &image.Info2D{
-					Format: format,
-					Width:  level.Width,
-					Height: level.Height,
-					Data:   image.NewID(level.Data.ResourceID(ctx, s)),
-				}
-			}
-			return &gfxapi.Texture2D{Levels: levels}, nil
+			return gfxapi.NewResourceData(gfxapi.NewTexture(&gfxapi.Texture_Cubemap{
+				Cubemap: &gfxapi.Cubemap{Levels: cubeMapLevels},
+			})), nil
 		}
+
+		levels := make([]*image.Info2D, len(t.Layers[0].Levels))
+		for i, level := range t.Layers[0].Levels {
+			levels[i] = &image.Info2D{
+				Format: format,
+				Width:  level.Width,
+				Height: level.Height,
+				Data:   image.NewID(level.Data.ResourceID(ctx, s)),
+			}
+		}
+		return gfxapi.NewResourceData(gfxapi.NewTexture(&gfxapi.Texture2D{Levels: levels})), nil
 	default:
 		return nil, &service.ErrDataUnavailable{Reason: messages.ErrNoTextureData(t.ResourceHandle())}
 	}
 }
 
 func (t *ImageObject) SetResourceData(ctx context.Context, at *path.Command,
-	data interface{}, resources gfxapi.ResourceMap, edits gfxapi.ReplaceCallback) error {
+	data *gfxapi.ResourceData, resources gfxapi.ResourceMap, edits gfxapi.ReplaceCallback) error {
 	return fmt.Errorf("SetResourceData is not supported for ImageObject")
 }
 
@@ -580,17 +577,17 @@ func (s *ShaderModuleObject) ResourceType(ctx context.Context) gfxapi.ResourceTy
 }
 
 // ResourceData returns the resource data given the current state.
-func (s *ShaderModuleObject) ResourceData(ctx context.Context, t *gfxapi.State) (interface{}, error) {
-	ctx = log.Enter(ctx, "Shader.ResourceData()")
+func (s *ShaderModuleObject) ResourceData(ctx context.Context, t *gfxapi.State) (*gfxapi.ResourceData, error) {
+	ctx = log.Enter(ctx, "ShaderModuleObject.ResourceData()")
 	words := s.Words.Read(ctx, nil, t, nil)
 	source := shadertools.DisassembleSpirvBinary(words)
-	return &gfxapi.Shader{Type: gfxapi.ShaderType_Spirv, Source: source}, nil
+	return gfxapi.NewResourceData(&gfxapi.Shader{Type: gfxapi.ShaderType_Spirv, Source: source}), nil
 }
 
 func (shader *ShaderModuleObject) SetResourceData(
 	ctx context.Context,
 	at *path.Command,
-	data interface{},
+	data *gfxapi.ResourceData,
 	resourceIDs gfxapi.ResourceMap,
 	edits gfxapi.ReplaceCallback) error {
 
@@ -635,12 +632,12 @@ func (shader *ShaderModuleObject) SetResourceData(
 	return fmt.Errorf("No atom to set data in")
 }
 
-func (a *VkCreateShaderModule) Replace(ctx context.Context, data interface{}) gfxapi.ResourceAtom {
+func (a *VkCreateShaderModule) Replace(ctx context.Context, data *gfxapi.ResourceData) gfxapi.ResourceAtom {
 	ctx = log.Enter(ctx, "VkCreateShaderModule.Replace()")
 	state := capture.NewState(ctx)
 	a.Mutate(ctx, state, nil)
 
-	shader := data.(*gfxapi.Shader)
+	shader := data.GetShader()
 	codeSlice := shadertools.AssembleSpirvText(shader.Source)
 	if codeSlice == nil {
 		return nil
@@ -684,12 +681,12 @@ func (a *VkCreateShaderModule) Replace(ctx context.Context, data interface{}) gf
 	return newAtom
 }
 
-func (a *RecreateShaderModule) Replace(ctx context.Context, data interface{}) gfxapi.ResourceAtom {
+func (a *RecreateShaderModule) Replace(ctx context.Context, data *gfxapi.ResourceData) gfxapi.ResourceAtom {
 	ctx = log.Enter(ctx, "RecreateShaderModule.Replace()")
 	state := capture.NewState(ctx)
 	a.Mutate(ctx, state, nil)
 
-	shader := data.(*gfxapi.Shader)
+	shader := data.GetShader()
 	codeSlice := shadertools.AssembleSpirvText(shader.Source)
 	if codeSlice == nil {
 		return nil

cmd/gapit/dump_shaders.go
@@ -85,12 +85,13 @@ func (verb *dumpShadersVerb) Run(ctx context.Context, flags flag.FlagSet) error 
 					continue
 				}
 				resourcePath := capture.Command(uint64(verb.At)).ResourceAfter(v.Id)
-				shaderData, err := client.Get(ctx, resourcePath.Path())
+				resourceData, err := client.Get(ctx, resourcePath.Path())
 				if err != nil {
 					log.E(ctx, "Could not get data for shader: %v %v", v, err)
 					continue
 				}
-				shaderSource := shaderData.(*gfxapi.Shader).GetSource()
+
+				shaderSource := resourceData.(*gfxapi.ResourceData).GetShader().GetSource()
 
 				f, err := os.Create(v.GetHandle())
 				if err != nil {

core/image/CMakeFiles.cmake
@@ -21,6 +21,7 @@ set(files
     astc.go
     atc.go
     convert.go
+    convertable.go
     decompress_test.go
     doc.go
     etc1.go

core/image/convertable.go
@@ -1,0 +1,25 @@
+// Copyright (C) 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package image
+
+import (
+	"context"
+)
+
+// Convertable is the interface implemented by types that can be converted to
+// a new image format.
+type Convertable interface {
+	ConvertTo(ctx context.Context, f *Format) (interface{}, error)
+}

core/image/image.go
@@ -25,8 +25,16 @@ import (
 	"github.com/google/gapid/gapis/database"
 )
 
-// ConvertTo returns this image Info converted to the format f.
-func (i *Info2D) ConvertTo(ctx context.Context, f *Format) (*Info2D, error) {
+// Interface compliance check
+var _ = Convertable((*Info2D)(nil))
+
+// ConvertTo implements the Convertable interface. It directly calls Convert.
+func (i *Info2D) ConvertTo(ctx context.Context, f *Format) (interface{}, error) {
+	return i.Convert(ctx, f)
+}
+
+// Convert returns this image Info converted to the format f.
+func (i *Info2D) Convert(ctx context.Context, f *Format) (*Info2D, error) {
 	id, err := database.Store(ctx, &ConvertResolvable{
 		Data:       i.Data,
 		Width:      i.Width,

gapic/src/main/com/google/gapid/image/FetchedImage.java
@@ -28,6 +28,7 @@ import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.gapid.proto.image.Image.Info2D;
 import com.google.gapid.proto.service.Service.Value;
+import com.google.gapid.proto.service.gfxapi.GfxAPI;
 import com.google.gapid.proto.service.gfxapi.GfxAPI.Cubemap;
 import com.google.gapid.proto.service.gfxapi.GfxAPI.CubemapLevel;
 import com.google.gapid.proto.service.gfxapi.GfxAPI.Texture2D;
@@ -59,9 +60,11 @@ public class FetchedImage implements MultiLevelImage {
 
   public static ListenableFuture<FetchedImage> load(Client client, Path.ResourceData imagePath) {
     return Futures.transformAsync(client.get(resourceInfo(imagePath)), value -> {
-      switch (value.getValCase()) {
-        case TEXTURE_2D: return load(client, imagePath, getFormat(value.getTexture2D()));
-        case CUBEMAP: return load(client, imagePath, getFormat(value.getCubemap()));
+      GfxAPI.ResourceData data = value.getResourceData();
+      GfxAPI.Texture texture = data.getTexture();
+      switch (texture.getTypeCase()) {
+        case TEXTURE_2D: return load(client, imagePath, getFormat(texture.getTexture2D()));
+        case CUBEMAP: return load(client, imagePath, getFormat(texture.getCubemap()));
         default:
           throw new UnsupportedOperationException("Unexpected resource type: " + value);
       }
@@ -71,9 +74,11 @@ public class FetchedImage implements MultiLevelImage {
   public static ListenableFuture<FetchedImage> load(
       Client client, Path.ResourceData imagePath, Images.Format format) {
     return Futures.transform(client.get(imageData(imagePath, format.format)), value -> {
-      switch (value.getValCase()) {
-        case TEXTURE_2D: return new FetchedImage(client, format, value.getTexture2D());
-        case CUBEMAP: return new FetchedImage(client, format, value.getCubemap());
+      GfxAPI.ResourceData data = value.getResourceData();
+      GfxAPI.Texture texture = data.getTexture();
+      switch (texture.getTypeCase()) {
+        case TEXTURE_2D: return new FetchedImage(client, format, texture.getTexture2D());
+        case CUBEMAP: return new FetchedImage(client, format, texture.getCubemap());
         default:
           throw new UnsupportedOperationException("Unexpected resource type: " + value);
       }

gapic/src/main/com/google/gapid/views/ShaderView.java
@@ -38,6 +38,7 @@ import com.google.gapid.models.Models;
 import com.google.gapid.models.Resources;
 import com.google.gapid.proto.core.pod.Pod;
 import com.google.gapid.proto.service.Service;
+import com.google.gapid.proto.service.gfxapi.GfxAPI;
 import com.google.gapid.proto.service.gfxapi.GfxAPI.Program;
 import com.google.gapid.proto.service.gfxapi.GfxAPI.ResourceType;
 import com.google.gapid.proto.service.gfxapi.GfxAPI.Shader;
@@ -123,8 +124,9 @@ public class ShaderView extends Composite
       Shader shader = (data == null) ? null : (Shader)data.resource;
       if (shader != null) {
         Service.Value value = Service.Value.newBuilder()
-            .setShader(shader.toBuilder()
-                .setSource(src))
+            .setResourceData(GfxAPI.ResourceData.newBuilder()
+                .setShader(shader.toBuilder()
+                    .setSource(src)))
             .build();
         Rpc.listen(client.set(data.getPath(models.atoms), value),
             new UiCallback<Path.Any, Path.Capture>(this, LOG) {
@@ -169,7 +171,7 @@ public class ShaderView extends Composite
       @Override
       protected ShaderPanel.Source onRpcThread(Result<Service.Value> result)
           throws RpcException, ExecutionException {
-        Shader shader = result.get().getShader();
+        Shader shader = result.get().getResourceData().getShader();
         data.resource = shader;
         return ShaderPanel.Source.of(shader);
       }
@@ -188,7 +190,7 @@ public class ShaderView extends Composite
       @Override
       protected ShaderPanel.Source[] onRpcThread(Result<Service.Value> result)
           throws RpcException, ExecutionException {
-        Program program = result.get().getProgram();
+        Program program = result.get().getResourceData().getProgram();
         data.resource = program;
         onProgramLoaded.accept(program);
         return ShaderPanel.Source.of(program);

commit 73c23389afdcf42971b652e8b47f9f0f8a30eea2

Fix "panic: *gfxapi.Texture_Cubemap is not a Texture type"
Panic introduced by b03855a

gapis/gfxapi/gles/resources.go
@@ -107,9 +107,7 @@ func (t *Texture) ResourceData(ctx context.Context, s *gfxapi.State) (*gfxapi.Re
 				}
 			}
 		}
-		return gfxapi.NewResourceData(gfxapi.NewTexture(&gfxapi.Texture_Cubemap{
-			Cubemap: &gfxapi.Cubemap{Levels: levels},
-		})), nil
+		return gfxapi.NewResourceData(gfxapi.NewTexture(&gfxapi.Cubemap{Levels: levels})), nil
 
 	default:
 		return nil, &service.ErrDataUnavailable{Reason: messages.ErrNoTextureData(t.ResourceHandle())}

gapis/gfxapi/vulkan/resources.go
@@ -526,9 +526,7 @@ func (t *ImageObject) ResourceData(ctx context.Context, s *gfxapi.State) (*gfxap
 					}
 				}
 			}
-			return gfxapi.NewResourceData(gfxapi.NewTexture(&gfxapi.Texture_Cubemap{
-				Cubemap: &gfxapi.Cubemap{Levels: cubeMapLevels},
-			})), nil
+			return gfxapi.NewResourceData(gfxapi.NewTexture(&gfxapi.Cubemap{Levels: cubeMapLevels})), nil
 		}
 
 		levels := make([]*image.Info2D, len(t.Layers[0].Levels))