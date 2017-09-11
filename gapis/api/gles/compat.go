e9566c39181bef93dec34ff9f2793fc407b41aab
David Srbecký <dsrbecky@gmail.com>
2017-09-11 17:57:57 +0100

    Implement GL_OVR_multiview_multisampled_render_to_texture
    Fixes #1090

--- gapis/api/gles/compat.go
+++ gapis/api/gles/compat.go
@@ -1050,7 +1050,20 @@ func compat(ctx context.Context, device *device.Instance) (transform.Transformer
 			{
 				cmd.Mutate(ctx, id, s, nil /* no builder, just mutate */)
 				// Translate it to the non-multiview version, but do not modify state,
-				// otherwise we would loose the knowledge about view count.
+				// otherwise we would lose the knowledge about view count.
+				out.MutateAndWrite(ctx, dID, cb.Custom(func(ctx context.Context, s *api.State, b *builder.Builder) error {
+					cb.GlFramebufferTextureLayer(cmd.Target, cmd.Attachment, cmd.Texture, cmd.Level, cmd.BaseViewIndex).Call(ctx, s, b)
+					return nil
+				}))
+				return
+			}
+
+		case *GlFramebufferTextureMultisampleMultiviewOVR:
+			{
+				// TODO: Support multi-sample rendering.
+				cmd.Mutate(ctx, id, s, nil /* no builder, just mutate */)
+				// Translate it to the non-multiview version, but do not modify state,
+				// otherwise we would lose the knowledge about view count.
 				out.MutateAndWrite(ctx, dID, cb.Custom(func(ctx context.Context, s *api.State, b *builder.Builder) error {
 					cb.GlFramebufferTextureLayer(cmd.Target, cmd.Attachment, cmd.Texture, cmd.Level, cmd.BaseViewIndex).Call(ctx, s, b)
 					return nil
