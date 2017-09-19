122579c94f1cf6478bfef42818fdc27d06b95da1
Pascal Muetschard <pmuetschard@google.com>
2017-09-19 15:13:20 -0700

    Move the RPC initiation off of the UI thread.

--- gapic/src/main/com/google/gapid/server/Client.java
+++ gapic/src/main/com/google/gapid/server/Client.java
@@ -21,6 +21,7 @@ import static java.util.logging.Level.FINE;
 
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.SettableFuture;
 import com.google.gapid.models.Strings;
 import com.google.gapid.proto.log.Log;
 import com.google.gapid.proto.service.Service;
@@ -45,10 +46,12 @@ import com.google.gapid.proto.service.path.Path;
 import com.google.gapid.proto.stringtable.Stringtable;
 import com.google.gapid.rpc.RpcException;
 import com.google.gapid.util.Paths;
+import com.google.gapid.util.Scheduler;
 import com.google.protobuf.ByteString;
 
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.logging.Logger;
 
@@ -65,134 +68,127 @@ public class Client {
   }
 
   public ListenableFuture<ServerInfo> getSeverInfo() {
-    LOG.log(FINE, "RPC->getServerInfo()");
-    Stack stack = new Stack(() -> "RPC->getServerInfo()");
-    return Futures.transformAsync(
-        client.getServerInfo(GetServerInfoRequest.newBuilder().build()),
-        in -> immediateFuture(throwIfError(in.getInfo(), in.getError(), stack))
-    );
+    return call(() -> "RPC->getServerInfo()",
+        stack -> Futures.transformAsync(
+            client.getServerInfo(GetServerInfoRequest.getDefaultInstance()),
+            in -> immediateFuture(throwIfError(in.getInfo(), in.getError(), stack))));
   }
 
   public ListenableFuture<Release> checkForUpdates(boolean includePrereleases) {
-    LOG.log(FINE, "RPC->checkForUpdates({0})", includePrereleases);
-    Stack stack = new Stack(() -> String.format("RPC->checkForUpdates(%b)", includePrereleases));
-    return Futures.transformAsync(
-        client.checkForUpdates(CheckForUpdatesRequest.newBuilder()
-            .setIncludePrereleases(includePrereleases).build()),
-        in -> immediateFuture(throwIfError(in.getRelease(), in.getError(), stack))
-    );
+    return call(() -> String.format("RPC->checkForUpdates(%b)", includePrereleases),
+        stack -> Futures.transformAsync(
+            client.checkForUpdates(CheckForUpdatesRequest.newBuilder()
+                .setIncludePrereleases(includePrereleases)
+                .build()),
+            in -> immediateFuture(throwIfError(in.getRelease(), in.getError(), stack))));
   }
 
   public ListenableFuture<Value> get(Path.Any path) {
-    LOG.log(FINE, "RPC->get({0})", path);
-    Stack stack = new Stack(() -> String.format("RPC->get(%s)", shortDebugString(path)));
-    return Futures.transformAsync(
-        client.get(GetRequest.newBuilder().setPath(path).build()),
-        in -> immediateFuture(throwIfError(in.getValue(), in.getError(), stack))
-    );
+    return call(() -> String.format("RPC->get(%s)", shortDebugString(path)),
+        stack -> Futures.transformAsync(
+            client.get(GetRequest.newBuilder()
+                .setPath(path)
+                .build()),
+            in -> immediateFuture(throwIfError(in.getValue(), in.getError(), stack))));
   }
 
   public ListenableFuture<Path.Any> set(Path.Any path, Service.Value value) {
-    LOG.log(FINE, "RPC->set({0}, {1})", new Object[] { path, value });
-    Stack stack = new Stack(
-        () -> String.format("RPC->set(%s, %s)", shortDebugString(path), shortDebugString(value)));
-    return Futures.transformAsync(
-        client.set(SetRequest.newBuilder().setPath(path).setValue(value).build()),
-        in -> immediateFuture(throwIfError(in.getPath(), in.getError(), stack))
-    );
+    return call(
+        () -> String.format("RPC->set(%s, %s)", shortDebugString(path), shortDebugString(value)),
+        stack -> Futures.transformAsync(
+            client.set(SetRequest.newBuilder()
+                .setPath(path)
+                .setValue(value)
+                .build()),
+            in -> immediateFuture(throwIfError(in.getPath(), in.getError(), stack))));
   }
 
   public ListenableFuture<Path.Any> follow(Path.Any path) {
-    LOG.log(FINE, "RPC->follow({0})", path);
-    Stack stack = new Stack(() -> String.format("RPC->follow(%s)", shortDebugString(path)));
-    return Futures.transformAsync(
-        client.follow(FollowRequest.newBuilder().setPath(path).setPath(path).build()),
-        in -> immediateFuture(throwIfError(in.getPath(), in.getError(), stack))
-    );
+    return call(() -> String.format("RPC->follow(%s)", shortDebugString(path)),
+        stack -> Futures.transformAsync(
+            client.follow(FollowRequest.newBuilder()
+                .setPath(path)
+                .build()),
+            in -> immediateFuture(throwIfError(in.getPath(), in.getError(), stack))));
   }
 
   public ListenableFuture<List<Stringtable.Info>> getAvailableStringTables() {
-    LOG.log(FINE, "RPC->getAvailableStringTables()");
-    Stack stack = new Stack(() -> "RPC->getAvailableStringTables()");
-    return Futures.transformAsync(
-        client.getAvailableStringTables(GetAvailableStringTablesRequest.newBuilder().build()),
-        in -> immediateFuture(throwIfError(in.getTables(), in.getError(), stack).getListList())
-    );
+    return call(() -> "RPC->getAvailableStringTables()",
+        stack -> Futures.transformAsync(
+          client.getAvailableStringTables(GetAvailableStringTablesRequest.getDefaultInstance()),
+          in -> immediateFuture(throwIfError(in.getTables(), in.getError(), stack).getListList())));
   }
 
   public ListenableFuture<Stringtable.StringTable> getStringTable(Stringtable.Info info) {
-    LOG.log(FINE, "RPC->getStringTable({0})", info);
-    Stack stack = new Stack(() -> String.format("RPC->getStringTable(%s)", shortDebugString(info)));
-    return Futures.transformAsync(
-        client.getStringTable(GetStringTableRequest.newBuilder().setTable(info).build()),
-        in -> immediateFuture(throwIfError(in.getTable(), in.getError(), stack))
-    );
+    return call(() -> String.format("RPC->getStringTable(%s)", shortDebugString(info)),
+        stack -> Futures.transformAsync(
+            client.getStringTable(GetStringTableRequest.newBuilder()
+                .setTable(info)
+                .build()),
+            in -> immediateFuture(throwIfError(in.getTable(), in.getError(), stack))));
   }
+
   public ListenableFuture<Path.Capture> importCapture(byte[] data) {
-    LOG.log(FINE, "RPC->importCapture(<{0} bytes>)", data.length);
-    Stack stack = new Stack(() -> String.format("RPC->importCapture(<%d bytes>)", data.length));
-    return Futures.transformAsync(client.importCapture(
-        ImportCaptureRequest.newBuilder().setData(ByteString.copyFrom(data)).build()),
-        in -> immediateFuture(throwIfError(in.getCapture(), in.getError(), stack))
-    );
+    return call(() -> String.format("RPC->importCapture(<%d bytes>)", data.length),
+        stack -> Futures.transformAsync(client.importCapture(
+            ImportCaptureRequest.newBuilder()
+                .setData(ByteString.copyFrom(data))
+                .build()),
+            in -> immediateFuture(throwIfError(in.getCapture(), in.getError(), stack))));
   }
 
   public ListenableFuture<Path.Capture> loadCapture(String path) {
-    LOG.log(FINE, "RPC->loadCapture({0})", path);
-    Stack stack = new Stack(() -> String.format("RPC->loadCapture(%s)", path));
-    return Futures.transformAsync(
-        client.loadCapture(LoadCaptureRequest.newBuilder().setPath(path).build()),
-        in -> Futures.immediateFuture(throwIfError(in.getCapture(), in.getError(), stack))
-    );
+    return call(() -> String.format("RPC->loadCapture(%s)", path),
+        stack ->Futures.transformAsync(
+            client.loadCapture(LoadCaptureRequest.newBuilder()
+                .setPath(path)
+                .build()),
+            in -> Futures.immediateFuture(throwIfError(in.getCapture(), in.getError(), stack))));
   }
 
   public ListenableFuture<byte[]> exportCapture(Path.Capture path) {
-    LOG.log(FINE, "RPC->exportCapture({0})", path);
-    Stack stack = new Stack(() -> String.format("RPC->exportCapture(%s)", shortDebugString(path)));
-    return Futures.transformAsync(
-        client.exportCapture(ExportCaptureRequest.newBuilder().setCapture(path).build()),
-        in -> immediateFuture(throwIfError(in.getData().toByteArray(), in.getError(), stack))
-    );
+    return call(() -> String.format("RPC->exportCapture(%s)", shortDebugString(path)),
+        stack -> Futures.transformAsync(
+            client.exportCapture(ExportCaptureRequest.newBuilder()
+                .setCapture(path)
+                .build()),
+            in -> immediateFuture(throwIfError(in.getData().toByteArray(), in.getError(), stack))));
   }
 
   public ListenableFuture<List<Path.Device>> getDevices() {
-    LOG.log(FINE, "RPC->getDevices()");
-    Stack stack = new Stack(() -> "RPC->getDevices()");
-    return Futures.transformAsync(
-        client.getDevices(GetDevicesRequest.newBuilder().build()),
-        in -> immediateFuture(throwIfError(in.getDevices(), in.getError(), stack).getListList())
-    );
+    return call(() -> "RPC->getDevices()",
+        stack -> Futures.transformAsync(
+            client.getDevices(GetDevicesRequest.getDefaultInstance()),
+            in -> immediateFuture(throwIfError(in.getDevices(), in.getError(), stack)
+                .getListList())));
   }
 
   public ListenableFuture<List<Path.Device>> getDevicesForReplay(Path.Capture capture) {
-    LOG.log(FINE, "RPC->getDevicesForReplay({0})", capture);
-    Stack stack = new Stack(
-        () -> String.format("RPC->getDevicesForReplay(%s)", shortDebugString(capture)));
-    return Futures.transformAsync(client.getDevicesForReplay(
-        GetDevicesForReplayRequest.newBuilder().setCapture(capture).build()),
-        in -> immediateFuture(throwIfError(in.getDevices(), in.getError(), stack).getListList())
-    );
+    return call(() -> String.format("RPC->getDevicesForReplay(%s)", shortDebugString(capture)),
+        stack -> Futures.transformAsync(
+            client.getDevicesForReplay(GetDevicesForReplayRequest.newBuilder()
+              .setCapture(capture)
+              .build()),
+            in -> immediateFuture(throwIfError(in.getDevices(), in.getError(), stack)
+                .getListList())));
   }
 
   public ListenableFuture<Path.ImageInfo> getFramebufferAttachment(Path.Device device,
       Path.Command after, API.FramebufferAttachment attachment,
       Service.RenderSettings settings, Service.UsageHints hints) {
-    LOG.log(FINE, "RPC->getFramebufferAttachment({0}, {1}, {2}, {3}, {4})",
-        new Object[] { device, after, attachment, settings, hints });
-    Stack stack = new Stack(() ->
-      String.format("RPC->getFramebufferAttachment(%s, %s, %s, %s, %s)",
-          shortDebugString(device), shortDebugString(after), attachment, shortDebugString(settings),
-          shortDebugString(hints)));
-    return Futures.transformAsync(
-        client.getFramebufferAttachment(GetFramebufferAttachmentRequest.newBuilder()
-            .setDevice(device)
-            .setAfter(after)
-            .setAttachment(attachment)
-            .setSettings(settings)
-            .setHints(hints)
-            .build()),
-        in -> immediateFuture(throwIfError(in.getImage(), in.getError(), stack))
-    );
+    return call(
+        () -> String.format("RPC->getFramebufferAttachment(%s, %s, %s, %s, %s)",
+            shortDebugString(device), shortDebugString(after), attachment,
+            shortDebugString(settings), shortDebugString(hints)),
+        stack -> Futures.transformAsync(
+            client.getFramebufferAttachment(GetFramebufferAttachmentRequest.newBuilder()
+                .setDevice(device)
+                .setAfter(after)
+                .setAttachment(attachment)
+                .setSettings(settings)
+                .setHints(hints)
+                .build()),
+            in -> immediateFuture(throwIfError(in.getImage(), in.getError(), stack))));
   }
 
   public ListenableFuture<Void> streamLog(Consumer<Log.Message> onLogMessage) {
@@ -204,6 +200,19 @@ public class Client {
       Service.FindRequest request, Consumer<Service.FindResponse> onResult) {
     LOG.log(FINE, "RPC->find({0})", request);
     return client.streamSearch(request, onResult);
+  }
+
+  private static <V> ListenableFuture<V> call(
+      Supplier<String> stackMessage, Function<Stack, ListenableFuture<V>> call) {
+    SettableFuture<V> result = SettableFuture.create();
+    Stack stack = new Stack(stackMessage);
+    Scheduler.EXECUTOR.execute(() -> {
+      if (LOG.isLoggable(FINE)) {
+        LOG.log(FINE, stackMessage.get());
+      }
+      result.setFuture(call.apply(stack));
+    });
+    return result;
   }
 
   private static <V> V throwIfError(V value, Service.Error err, Stack stack) throws RpcException {
