9492539f1dea6aaf77ccc7ba0b5d6002d2c6b372
David Srbecký <dsrbecky@gmail.com>
2017-10-04 16:42:42 +0100

    Add support for PC-relative LDRi12

--- gapii/interceptor-lib/cc/lib/ARM/target_arm.cc
+++ gapii/interceptor-lib/cc/lib/ARM/target_arm.cc
@@ -114,6 +114,7 @@ static void *calculatePcRelativeAddressArm(void *data, size_t pc_offset,
 
   data_addr += pc_offset;  // Add the PC
   data_addr += 8;          // Add the 8 byte implicit offset
+  data_addr += offset;     // Add the offset
   return reinterpret_cast<void *>(data_addr);
 }
 
@@ -176,7 +177,6 @@ Error TargetARM::RewriteInstruction(const llvm::MCInst &inst,
     case llvm::ARM::LDR_PRE_REG:
     case llvm::ARM::LDR_POST_IMM:
     case llvm::ARM::LDR_POST_REG:
-    case llvm::ARM::LDRi12:
     case llvm::ARM::LDRH_PRE:
     case llvm::ARM::LDRH_POST:
     case llvm::ARM::LDRH:
@@ -273,6 +273,29 @@ Error TargetARM::RewriteInstruction(const llvm::MCInst &inst,
         codegen.AddInstruction(
             llvm::MCInstBuilder(llvm::ARM::tPOP).addImm(0).addImm(0).addReg(
                 scratch_reg));
+      } else {
+        codegen.AddInstruction(inst);
+      }
+      break;
+    }
+    case llvm::ARM::LDRi12: {
+      uint32_t Rt = inst.getOperand(0).getReg();
+      uint32_t Rn = inst.getOperand(1).getReg();
+      int64_t imm = inst.getOperand(2).getImm();
+      int64_t p   = inst.getOperand(3).getImm();
+      possible_end_of_function = (Rt == llvm::ARM::PC);
+
+      if (Rn == llvm::ARM::PC) {
+        void *load_source =
+            calculatePcRelativeAddressArm(data, offset, imm);
+        uint32_t load_data = 0;
+        memcpy(&load_data, load_source, sizeof(uint32_t));
+        codegen.AddInstruction(llvm::MCInstBuilder(llvm::ARM::LDRi12)
+                                   .addReg(Rt)
+                                   .addExpr(codegen.CreateDataExpr(load_data))
+                                   .addImm(0)
+                                   .addImm(p)
+                                   .addImm(0));
       } else {
         codegen.AddInstruction(inst);
       }
