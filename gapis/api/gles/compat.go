4c2748f25b37409dfeb027b223a6e623e9d28fa0
David Srbecký <dsrbecky@gmail.com>
2017-10-20 20:19:02 +0100

    Remove the SharedObjects indirection container.
    The container was just a hack around the fact we could not
    share pointer to the same map from two different contexts.
    Since that is possible now, clean up by removing the hack.

--- gapis/api/gles/compat.go
+++ gapis/api/gles/compat.go
@@ -282,7 +282,7 @@ func compat(ctx context.Context, device *device.Instance) (transform.Transformer
 					// It is helpful to remove those no-ops in case the replay driver does not support the target.
 					return
 				}
-			} else if !c.Objects.Shared.GeneratedNames.Buffers[cmd.Buffer] {
+			} else if !c.Objects.GeneratedNames.Buffers[cmd.Buffer] {
 				// glGenBuffers() was not used to generate the buffer. Legal in GLES 2.
 				tmp := s.AllocDataOrPanic(ctx, cmd.Buffer)
 				out.MutateAndWrite(ctx, dID, cb.GlGenBuffers(1, tmp.Ptr()).AddRead(tmp.Data()))
@@ -291,7 +291,7 @@ func compat(ctx context.Context, device *device.Instance) (transform.Transformer
 		case *GlBindTexture:
 			{
 				cmd := *cmd
-				if cmd.Texture != 0 && !c.Objects.Shared.GeneratedNames.Textures[cmd.Texture] {
+				if cmd.Texture != 0 && !c.Objects.GeneratedNames.Textures[cmd.Texture] {
 					// glGenTextures() was not used to generate the texture. Legal in GLES 2.
 					tmp := s.AllocDataOrPanic(ctx, cmd.Texture)
 					out.MutateAndWrite(ctx, dID, cb.GlGenTextures(1, tmp.Ptr()).AddRead(tmp.Data()))
@@ -359,7 +359,7 @@ func compat(ctx context.Context, device *device.Instance) (transform.Transformer
 				// TODO: We don't handle the case where the buffer is kept bound
 				// while the buffer is updated. It's an unlikely issue, but
 				// something that may break us.
-				if _, ok := c.Objects.Shared.Buffers[cmd.Buffer]; !ok {
+				if _, ok := c.Objects.Buffers[cmd.Buffer]; !ok {
 					return // Don't know what buffer this is referring to.
 				}
 
@@ -446,7 +446,7 @@ func compat(ctx context.Context, device *device.Instance) (transform.Transformer
 			return
 
 		case *GlCompileShader:
-			shader := c.Objects.Shared.Shaders.Get(cmd.Shader)
+			shader := c.Objects.Shaders.Get(cmd.Shader)
 			src := ""
 
 			if config.UseGlslang {
@@ -1129,7 +1129,7 @@ func compat(ctx context.Context, device *device.Instance) (transform.Transformer
 				// Forcefully get all uniform locations, so that we can remap for applications that
 				// just assume locations (in particular, apps tend to assume arrays are consecutive)
 				// TODO: We should warn the developers that the consecutive layout is not guaranteed.
-				prog := c.Objects.Shared.Programs[cmd.Program]
+				prog := c.Objects.Programs[cmd.Program]
 				for _, uniformIndex := range prog.ActiveUniforms.KeysSorted() {
 					uniform := prog.ActiveUniforms[uniformIndex]
 					for i := 0; i < int(uniform.ArraySize); i++ {
