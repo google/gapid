835e52cc9ee9875313c0b8d5b110cae8cc09611f
Ben Clayton <bclayton@google.com>
2017-10-27 17:09:37 +0100

    cmd/gapit: Give SxS video exceptions to undefined FBs.

--- cmd/gapit/sxs_video.go
+++ cmd/gapit/sxs_video.go
@@ -43,6 +43,7 @@ type videoFrame struct {
 	frameIndex    int
 	numDrawCalls  int
 	renderError   error
+	permitNoMatch bool
 	rendered      *image.NRGBA
 	observed      *image.NRGBA
 	difference    *image.NRGBA
@@ -61,6 +62,15 @@ func getVideoFrames(
 	videoFrames := []*videoFrame{}
 	w, h := 0, 0
 	frameIndex, numDrawCalls := 0, 0
+
+	// Some traces call eglSwapBuffers without actually drawing to or clearing
+	// the framebuffer. This is most common during loading screens.
+	// These would result in a failed comparison as the observed frame could
+	// be anything and the replayed frame will show the undefined framebuffer
+	// pattern.
+	// Permit the first run of frames to have no content.
+	permitNoMatch := true
+
 	var lastFrameEvent *path.Command
 	for _, e := range events {
 		switch e.Kind {
@@ -80,16 +90,20 @@ func getVideoFrames(
 			if int(fbo.Height) > h {
 				h = int(fbo.Height)
 			}
+
 			videoFrames = append(videoFrames, &videoFrame{
-				fbo:          fbo,
-				fboIndex:     fmt.Sprint(e.Command.Indices),
-				frameIndex:   frameIndex,
-				numDrawCalls: numDrawCalls,
-				command:      lastFrameEvent,
+				fbo:           fbo,
+				fboIndex:      fmt.Sprint(e.Command.Indices),
+				frameIndex:    frameIndex,
+				numDrawCalls:  numDrawCalls,
+				command:       lastFrameEvent,
+				permitNoMatch: permitNoMatch,
 			})
 		case service.EventKind_Clear:
+			permitNoMatch = false
 			lastFrameEvent = e.Command
 		case service.EventKind_DrawCall:
+			permitNoMatch = false
 			lastFrameEvent = e.Command
 			numDrawCalls++
 		case service.EventKind_LastInFrame:
@@ -278,8 +292,9 @@ func (verb *videoVerb) sxsVideoSource(
 
 		const threshold = 0.01
 		for _, v := range videoFrames {
-			if v.squareError > threshold {
-				return fmt.Errorf("FramebufferObservation did not match replayed framebuffer. Difference: %v%%", v.squareError*100)
+			if !v.permitNoMatch && v.squareError > threshold {
+				return fmt.Errorf("FramebufferObservation did not match replayed framebuffer at %v. Difference: %v%%",
+					v.command.Indices, v.squareError*100)
 			}
 		}
 		return nil
