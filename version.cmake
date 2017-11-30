d235c344a3cd73e2df63a559396b4825a5db58b4
Pascal Muetschard <pmuetschard@google.com>
2017-11-29 21:36:33 -0800

    Bump GAPID version to v0.9.3

--- version.cmake
+++ version.cmake
@@ -16,7 +16,7 @@
 # Increment these numbers immediately after releasing a new version.
 set(GAPID_VERSION_MAJOR 0)
 set(GAPID_VERSION_MINOR 9)
-set(GAPID_VERSION_POINT 2)
+set(GAPID_VERSION_POINT 3)
 
 if (NOT DEFINED GAPID_BUILD_NUMBER)
     set(GAPID_BUILD_NUMBER 0)
