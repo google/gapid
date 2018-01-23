a6d0f87cba6e68bbc857fb1a97ca424ae94a4624
Pascal Muetschard <pmuetschard@google.com>
2018-01-23 01:49:54 -0800

    Fix a comment.

--- cmd/llvm-build/main.go
+++ cmd/llvm-build/main.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Command llvm-build generated bazel build rules for the LLVM dependency.
+// Command llvm-build generates bazel build rules for the LLVM dependency.
 package main
 
 import (
