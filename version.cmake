dfb30ea34c82108f5a9b0cb459d84d4017a52733
Ben Clayton <bclayton@google.com>
2018-01-30 10:55:50 +0000

    Bump GAPID version to v1.1.0

--- version.cmake
+++ version.cmake
@@ -15,8 +15,8 @@
 # True source of GAPID versions.
 # Increment these numbers immediately after releasing a new version.
 set(GAPID_VERSION_MAJOR 1)
-set(GAPID_VERSION_MINOR 0)
-set(GAPID_VERSION_POINT 1)
+set(GAPID_VERSION_MINOR 1)
+set(GAPID_VERSION_POINT 0)
 
 if (NOT DEFINED GAPID_BUILD_NUMBER)
     set(GAPID_BUILD_NUMBER 0)
