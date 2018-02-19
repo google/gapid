a0711f5127fc5d4c4af9005f65a3d3343642918e
Ben Clayton <bclayton@google.com>
2018-02-19 19:26:14 +0000

    cmd/apic: Remove unused dependency generation logic
    With the switch to bazel this is no longer used.

--- cmd/apic/template.go
+++ cmd/apic/template.go
@@ -27,7 +27,6 @@ import (
 	"github.com/google/gapid/core/app/flags"
 	"github.com/google/gapid/core/log"
 	"github.com/google/gapid/core/os/file"
-	"github.com/google/gapid/gapil"
 	"github.com/google/gapid/gapil/template"
 )
 
@@ -44,20 +43,17 @@ func init() {
 type templateVerb struct {
 	Dir        string        `help:"The output directory"`
 	Tracer     string        `help:"The template function trace expression"`
-	Deps       string        `help:"The dependancies file to generate"`
-	Cmake      string        `help:"The cmake dependancies file to generate"`
 	Gopath     string        `help:"the go path to use when looking up packages"`
 	GlobalList flags.Strings `help:"A global value setting for the template"`
 	Search     file.PathList `help:"The set of paths to search for includes"`
 }
 
 func (v *templateVerb) Run(ctx context.Context, flags flag.FlagSet) error {
-	args := flags.Args()
-	if len(args) < 1 {
-		app.Usage(ctx, "Missing api file")
-		return nil
+	api, mappings, err := resolve(ctx, v.Search, flags)
+	if err != nil {
+		return err
 	}
-	apiName := args[0]
+	args := flags.Args()
 	if len(args) < 2 {
 		app.Usage(ctx, "Missing template file")
 		return nil
@@ -66,66 +62,25 @@ func (v *templateVerb) Run(ctx context.Context, flags flag.FlagSet) error {
 		build.Default.GOPATH = filepath.FromSlash(v.Gopath)
 	}
 	mainTemplate := args[1]
-	log.D(ctx, "Reading %v", apiName)
-	processor := gapil.NewProcessor()
-	if len(v.Search) > 0 {
-		processor.Loader = gapil.NewSearchLoader(v.Search)
-	}
-	compiled, errs := processor.Resolve(apiName)
-	for path := range processor.Parsed {
-		template.InputDep(path)
-	}
-	if err := gapil.CheckErrors(apiName, errs, maxErrors); err != nil {
-		return err
-	}
+	log.D(ctx, "Reading %v", api.Name())
 	options := template.Options{
 		Dir:     v.Dir,
-		APIFile: apiName,
+		APIFile: api.Name(),
 		Loader:  ioutil.ReadFile,
 		Globals: v.GlobalList.Strings(),
 		Tracer:  v.Tracer,
 	}
-	f, err := template.NewFunctions(ctx, compiled, processor.Mappings, options)
+	f, err := template.NewFunctions(ctx, api, mappings, options)
 	if err != nil {
 		return err
 	}
 	if err := f.Include(mainTemplate); err != nil {
 		return fmt.Errorf("%s: %s\n", mainTemplate, err)
 	}
-	v.writeDeps(ctx)
-	v.writeCMake(ctx)
 	return nil
 }
 
 func cwd() string {
 	p, _ := os.Getwd()
 	return p
-}
-
-func (v *templateVerb) writeDeps(ctx context.Context) error {
-	if len(v.Deps) == 0 {
-		return nil
-	}
-	log.D(ctx, "Writing deps %v", v.Deps)
-	file, err := os.Create(v.Deps)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-	template.WriteDeps(ctx, file)
-	return file.Close()
-}
-
-func (v *templateVerb) writeCMake(ctx context.Context) error {
-	if len(v.Cmake) == 0 {
-		return nil
-	}
-	log.D(ctx, "Writing cmake %v", v.Cmake)
-	file, err := os.Create(v.Cmake)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-	template.WriteCMake(ctx, file)
-	return file.Close()
 }
