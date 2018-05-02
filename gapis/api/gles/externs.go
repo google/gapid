google/gapid

commit eca98c3862f32faf8f13fe3968eb7c9b8591a1ba

Add Size field to Slices.
Note: This is a file format breaking change.

We've switched between using Size or Count a couple of times now, and
we currently only serialize Count, however the new compiler relies on
Size information. Converting between these two requires the memory
layout with is awkward to get at when deserializing.

Given that we've already broken file formats between v0.1.3 and the
next release, I've simply added the field and bumped the file format
version again.

gapis/api/gles/externs.go
@@ -46,7 +46,7 @@ func (e externs) mapMemory(slice memory.Slice) {
 		switch e.cmd.(type) {
 		case *GlMapBufferRange, *GlMapBufferRangeEXT, *GlMapBufferOES, *GlMapBuffer:
 			// Base address is on the stack.
-			b.MapMemory(slice.Range(e.s.MemoryLayout))
+			b.MapMemory(memory.Range{Base: slice.Base(), Size: slice.Size()})
 
 		default:
 			log.E(ctx, "mapBuffer extern called for unsupported command: %v", e.cmd)
@@ -56,7 +56,7 @@ func (e externs) mapMemory(slice memory.Slice) {
 
 func (e externs) unmapMemory(slice memory.Slice) {
 	if b := e.b; b != nil {
-		b.UnmapMemory(slice.Range(e.s.MemoryLayout))
+		b.UnmapMemory(memory.Range{Base: slice.Base(), Size: slice.Size()})
 	}
 }
 

gapis/api/vulkan/externs.go
@@ -42,8 +42,8 @@ func (e externs) hasDynamicProperty(info VkPipelineDynamicStateCreateInfoᶜᵖ,
 		return false
 	}
 	l := e.s.MemoryLayout
-	dynamic_state_info := info.Slice(uint64(0), uint64(1), l).Index(uint64(0), l).MustRead(e.ctx, e.cmd, e.s, e.b)
-	states := dynamic_state_info.PDynamicStates.Slice(uint64(uint32(0)), uint64(dynamic_state_info.DynamicStateCount), l).MustRead(e.ctx, e.cmd, e.s, e.b)
+	dynamic_state_info := info.Slice(0, 1, l).Index(0).MustRead(e.ctx, e.cmd, e.s, e.b)
+	states := dynamic_state_info.PDynamicStates.Slice(0, uint64(dynamic_state_info.DynamicStateCount), l).MustRead(e.ctx, e.cmd, e.s, e.b)
 	for _, s := range states {
 		if s == state {
 			return true
@@ -58,7 +58,7 @@ func (e externs) mapMemory(value Voidᵖᵖ, slice memory.Slice) {
 		switch e.cmd.(type) {
 		case *VkMapMemory:
 			b.Load(protocol.Type_AbsolutePointer, value.value(e.b, e.cmd, e.s))
-			b.MapMemory(slice.Range(e.s.MemoryLayout))
+			b.MapMemory(memory.Range{Base: slice.Base(), Size: slice.Size()})
 		default:
 			log.E(ctx, "mapBuffer extern called for unsupported command: %v", e.cmd)
 		}
@@ -165,7 +165,7 @@ func (e externs) postBindSparse(binds *QueuedSparseBinds) {
 
 func (e externs) unmapMemory(slice memory.Slice) {
 	if b := e.b; b != nil {
-		b.UnmapMemory(slice.Range(e.s.MemoryLayout))
+		b.UnmapMemory(memory.Range{Base: slice.Base(), Size: slice.Size()})
 	}
 }
 
@@ -182,7 +182,7 @@ func (e externs) readMappedCoherentMemory(memory_handle VkDeviceMemory, offset_i
 
 	writeRngList := e.s.Memory.ApplicationPool().Slice(absSrcMemRng).ValidRanges()
 	for _, r := range writeRngList {
-		mem.Data.Slice(dstStart+r.Base, dstStart+r.Base+r.Size, l).Copy(
+		mem.Data.Slice(dstStart+r.Base, dstStart+r.Base+r.Size).Copy(
 			e.ctx, U8ᵖ(mem.MappedLocation).Slice(srcStart+r.Base, srcStart+r.Base+r.Size, l), e.cmd, e.s, e.b)
 	}
 }
@@ -193,7 +193,7 @@ func (e externs) numberOfPNext(pNext Voidᶜᵖ) uint32 {
 	counter := uint32(0)
 	for (pNext) != (Voidᶜᵖ{}) {
 		counter++
-		pNext = Voidᶜᵖᵖ(pNext).Slice(uint64(0), uint64(2), l).Index(uint64(1), l).MustRead(e.ctx, e.cmd, e.s, e.b)
+		pNext = Voidᶜᵖᵖ(pNext).Slice(0, 2, l).Index(1).MustRead(e.ctx, e.cmd, e.s, e.b)
 	}
 	return counter
 }

gapii/cc/spy_base.cpp
@@ -23,7 +23,7 @@
 
 // CurrentCaptureVersion is incremented on breaking changes to the capture format.
 // NB: Also update equally named field in capture.go
-static const int CurrentCaptureVersion = 2;
+static const int CurrentCaptureVersion = 3;
 
 using core::Interval;
 

gapii/cc/vulkan_mid_execution.cpp
@@ -344,7 +344,7 @@ void VulkanSpy::prepareGPUBuffers(CallObserver *observer, PackEncoder *group,
   for (auto &mem : mState.DeviceMemories) {
     auto &memory = mem.second;
     memory->mData = gapil::Slice<uint8_t>::create(
-        create_virtual_pool(memory->mAllocationSize));
+        create_virtual_pool(memory->mAllocationSize), false);
     gpu_pools->insert(memory->mData.pool_id());
     if (memory->mMappedLocation != nullptr) {
       if (subIsMemoryCoherent(nullptr, nullptr, memory)) {
@@ -587,7 +587,7 @@ void VulkanSpy::prepareGPUBuffers(CallObserver *observer, PackEncoder *group,
               level_size(image_info.mExtent, image_info.mFormat, lev.first,
                          a.first);
           level->mData = gapil::Slice<uint8_t>::create(
-              create_virtual_pool(e.level_size));
+              create_virtual_pool(e.level_size), false);
           gpu_pools->insert(level->mData.pool_id());
         }
       }

gapil/compiler/builtins.go
@@ -52,10 +52,11 @@ const (
 
 // Field names for the slice_t runtime type.
 const (
-	SlicePool = "pool"
-	SliceRoot = "root"
-	SliceBase = "base"
-	SliceSize = "size"
+	SlicePool  = "pool"
+	SliceRoot  = "root"
+	SliceBase  = "base"
+	SliceSize  = "size"
+	SliceCount = "count"
 )
 
 // Field names for the pool_t runtime type.

gapil/compiler/cast.go
@@ -50,7 +50,7 @@ func (c *C) doCast(s *S, dstTy, srcTy semantic.Type, v *codegen.Value) *codegen.
 		pool := v.Extract(SlicePool)
 		count := s.Div(size, s.SizeOf(c.T.Storage(srcSliceTy.To)))
 		size = s.Mul(count, s.SizeOf(c.T.Storage(dstSliceTy.To)))
-		return c.buildSlice(s, root, base, size, pool)
+		return c.buildSlice(s, root, base, size, count, pool)
 	default:
 		return v.Cast(c.T.Target(dstTy)) // TODO: storage vs memory.
 	}

gapil/compiler/compiler.go
@@ -77,8 +77,8 @@ type C struct {
 		applyReads      codegen.Function
 		applyWrites     codegen.Function
 		freePool        codegen.Function
-		makeSlice       codegen.Function
 		copySlice       codegen.Function
+		makePool        codegen.Function
 		pointerToSlice  codegen.Function
 		pointerToString codegen.Function
 		sliceToString   codegen.Function
@@ -194,8 +194,8 @@ func (c *C) compile() {
 	c.callbacks.applyReads = c.M.ParseFunctionSignature(C.GoString(C.gapil_apply_reads_sig))
 	c.callbacks.applyWrites = c.M.ParseFunctionSignature(C.GoString(C.gapil_apply_writes_sig))
 	c.callbacks.freePool = c.M.ParseFunctionSignature(C.GoString(C.gapil_free_pool_sig))
-	c.callbacks.makeSlice = c.M.ParseFunctionSignature(C.GoString(C.gapil_make_slice_sig))
 	c.callbacks.copySlice = c.M.ParseFunctionSignature(C.GoString(C.gapil_copy_slice_sig))
+	c.callbacks.makePool = c.M.ParseFunctionSignature(C.GoString(C.gapil_make_pool_sig))
 	c.callbacks.pointerToSlice = c.M.ParseFunctionSignature(C.GoString(C.gapil_pointer_to_slice_sig))
 	c.callbacks.pointerToString = c.M.ParseFunctionSignature(C.GoString(C.gapil_pointer_to_string_sig))
 	c.callbacks.sliceToString = c.M.ParseFunctionSignature(C.GoString(C.gapil_slice_to_string_sig))
@@ -258,16 +258,22 @@ func (c *C) Build(f codegen.Function, do func(*S)) {
 }
 
 // MakeSlice creates a new slice of the given size in bytes.
-func (c *C) MakeSlice(s *S, size *codegen.Value) *codegen.Value {
+func (c *C) MakeSlice(s *S, size, count *codegen.Value) *codegen.Value {
 	dstPtr := s.Local("dstPtr", c.T.Sli)
-	c.MakeSliceAt(s, size, dstPtr)
+	c.MakeSliceAt(s, size, count, dstPtr)
 	return dstPtr.Load()
 }
 
 // MakeSliceAt creates a new slice of the given size in bytes at the given
 // slice pointer.
-func (c *C) MakeSliceAt(s *S, size, dstPtr *codegen.Value) {
-	s.Call(c.callbacks.makeSlice, s.Ctx, size, dstPtr)
+func (c *C) MakeSliceAt(s *S, size, count, dstPtr *codegen.Value) {
+	pool := s.Call(c.callbacks.makePool, s.Ctx, size)
+	buf := pool.Index(0, PoolBuffer).Load()
+	dstPtr.Index(0, SlicePool).Store(pool)
+	dstPtr.Index(0, SliceRoot).Store(buf)
+	dstPtr.Index(0, SliceBase).Store(buf)
+	dstPtr.Index(0, SliceSize).Store(size)
+	dstPtr.Index(0, SliceCount).Store(count)
 }
 
 // CopySlice copies the contents of slice src to dst.

gapil/compiler/expressions.go
@@ -283,8 +283,11 @@ func (c *C) create(s *S, e *semantic.Create) *codegen.Value {
 
 func (c *C) clone(s *S, e *semantic.Clone) *codegen.Value {
 	src := c.expression(s, e.Slice)
-	size := src.Extract(SliceSize)
-	dst := c.MakeSlice(s, size)
+	srcSize := src.Extract(SliceSize)
+	elSize := s.SizeOf(c.T.Storage(e.Type.To))
+	dstCount := s.Div(srcSize, elSize)
+	dstSize := s.Mul(dstCount, elSize)
+	dst := c.MakeSlice(s, dstSize, dstCount)
 	c.CopySlice(s, dst, src)
 	c.deferRelease(s, dst, e.Type)
 	return dst
@@ -334,8 +337,7 @@ func (c *C) length(s *S, e *semantic.Length) *codegen.Value {
 	var l *codegen.Value
 	switch ty := semantic.Underlying(e.Object.ExpressionType()).(type) {
 	case *semantic.Slice:
-		size := o.Extract(SliceSize)
-		l = s.Div(size, s.SizeOf(c.T.Storage(ty.To)))
+		l = o.Extract(SliceCount)
 	case *semantic.Map:
 		l = o.Index(0, MapCount).Load()
 	case *semantic.Builtin:
@@ -366,9 +368,7 @@ func (c *C) make(s *S, e *semantic.Make) *codegen.Value {
 	elTy := c.T.Storage(e.Type.To)
 	count := c.expression(s, e.Size).Cast(c.T.Uint64)
 	size := s.Mul(count, s.SizeOf(elTy))
-	slicePtr := s.Local("slicePtr", c.T.Sli)
-	s.Call(c.callbacks.makeSlice, s.Ctx, size, slicePtr)
-	slice := slicePtr.Load()
+	slice := c.MakeSlice(s, size, count)
 	c.deferRelease(s, slice, e.Type)
 	return slice
 }
@@ -434,7 +434,7 @@ func (c *C) pointerRange(s *S, e *semantic.PointerRange) *codegen.Value {
 	count := s.Sub(end, start).SetName("count")
 	size := s.Mul(count, s.SizeOf(elTy)).Cast(c.T.Uint64).SetName("size")
 	slicePtr := s.Local("slicePtr", c.T.Sli)
-	s.Call(c.callbacks.pointerToSlice, s.Ctx, p, offset, size, slicePtr)
+	s.Call(c.callbacks.pointerToSlice, s.Ctx, p, offset, size, count, slicePtr)
 	slice := slicePtr.Load()
 	c.deferRelease(s, slice, e.Type)
 	return slice
@@ -516,6 +516,7 @@ func (c *C) sliceRange(s *S, e *semantic.SliceRange) *codegen.Value {
 	end := base.Index(to).SetName("slice_end")                                        // T*
 	size := s.Sub(end.Cast(c.T.Uint64), start.Cast(c.T.Uint64)).SetName("slice_size") // u64
 
+	slice = slice.Insert(SliceCount, s.Sub(to, from))
 	slice = slice.Insert(SliceSize, size)
 	slice = slice.Insert(SliceBase, start.Cast(c.T.Uint8Ptr))
 	// TODO: Check sub-slice is within original slice bounds.

commit dcc49ddcf046892d75754be49d841fa100aee98f

gles: Fix for GPU texture readbacks
The size field of slices was not being set.
This got broken in eca98c386

gapis/api/gles/externs.go
@@ -79,7 +79,7 @@ func (e externs) GetEGLImageData(id EGLImageKHR, _ GLsizei, _ GLsizei) {
 			for _, img := range ei.Images.Range() {
 				poolID, pool := e.s.Memory.New()
 				pool.Write(0, memory.Resource(d.ID, d.Size))
-				data := U8ˢ{pool: poolID, count: d.Size}
+				data := U8ˢ{pool: poolID, count: d.Size, size: d.Size}
 				img.Width = GLsizei(d.Width)
 				img.Height = GLsizei(d.Height)
 				img.Data = data
@@ -185,7 +185,7 @@ func (e externs) ReadGPUTextureData(texture *Texture, level, layer GLint) U8ˢ {
 	device := registry.DefaultDevice() // TODO: Device selection.
 	if device == nil {
 		log.W(e.ctx, "No device found for GPU texture read")
-		return U8ˢ{count: uint64(size), pool: poolID}
+		return U8ˢ{pool: poolID, count: uint64(size), size: uint64(size)}
 	}
 	dataID, err := database.Store(e.ctx, &ReadGPUTextureDataResolveable{
 		Capture:    path.NewCapture(capture.Get(e.ctx).Id.ID()),
@@ -203,5 +203,5 @@ func (e externs) ReadGPUTextureData(texture *Texture, level, layer GLint) U8ˢ {
 	}
 	data := memory.Resource(dataID, uint64(size))
 	dst.Write(0, data)
-	return U8ˢ{count: uint64(size), pool: poolID}
+	return U8ˢ{pool: poolID, count: uint64(size), size: uint64(size)}
 }