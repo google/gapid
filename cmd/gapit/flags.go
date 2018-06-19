9661ceaca7c43a420dfec48c816234d274a7a828
Sean Purcell <scppurcell@google.com>
2018-06-19 11:25:25 -0400

    Add overdraw options to gapit screenshot to display results

--- cmd/gapit/flags.go
+++ cmd/gapit/flags.go
@@ -238,12 +238,16 @@ type (
 		ADB         string         `help:"Path to the adb executable; leave empty to search the environment"`
 	}
 	ScreenshotFlags struct {
-		Gapis GapisFlags
-		Gapir GapirFlags
-		At    flags.U64Slice `help:"command/subcommand index for the screenshot"`
-		Frame int64          `help:"frame index for the screenshot. Empty for last"`
-		Out   string         `help:"output image file (default 'screenshot.png')"`
-		NoOpt bool           `help:"disables optimization of the replay stream"`
+		Gapis    GapisFlags
+		Gapir    GapirFlags
+		At       flags.U64Slice `help:"command/subcommand index for the screenshot"`
+		Frame    int64          `help:"frame index for the screenshot. Empty for last"`
+		Out      string         `help:"output image file (default 'screenshot.png')"`
+		NoOpt    bool           `help:"disables optimization of the replay stream"`
+		Overdraw bool           `help:"renders the overdraw instead of the colour framebuffer"`
+		Max      struct {
+			Overdraw int `help:"the amount of overdraw to map to white in the output"`
+		}
 		CommandFilterFlags
 	}
 	UnpackFlags struct {
