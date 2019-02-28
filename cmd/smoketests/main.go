f1a77de1804ec08fffedba6429e690956c3256d6
Hugues Evrard <hevrard@users.noreply.github.com>
2019-02-28 11:25:22 +0100

    Add new smoketests command (#2619)
    * Add gapit smoke_test verb
    
    Gapit smoke_test runs a series of gapit commands on traces. It uses the
    gapit command found in PATH, so it can be used to test any gapid build.
    
    * Add small sample test trace
    
    * Avoid '_test' suffix in smoke_test
    
    The '_test' suffix is special in Go, used for tests of a given module.
    Smoke tests is a regular module, hence the renaming.
    
    * Add new command smoketests
    
    * Remove gapit verb smoke_tests
    
    * Refactor for idiomatic code and lisibility of test commads

---
+++ cmd/smoketests/main.go
@@ -0,0 +1,177 @@
+// Copyright (C) 2019 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// The smoketests command runs a series of smoke tests.
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"github.com/google/gapid/core/app"
+	"github.com/google/gapid/core/log"
+)
+
+var (
+	path = flag.String("traces", "traces", "The directory containing traces to run smoke tests on")
+)
+
+func main() {
+	app.ShortHelp = "smoketests runs a series of smoke tests"
+	app.Name = "smoketests"
+	app.Run(run)
+}
+
+func run(ctx context.Context) error {
+
+	// Check argument
+	filemode, err := os.Lstat(*path)
+	if err != nil {
+		return err
+	}
+	if !filemode.IsDir() {
+		return errors.New("Path given in argument is not a directory")
+	}
+	tracedir := *path
+
+	// Record starting working directory
+	startwd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	// Create temporary directory
+	tmpdir, err := ioutil.TempDir(startwd, "smoketests.")
+	if err != nil {
+		return err
+	}
+
+	// For each trace, run gapit tests
+	if !filepath.IsAbs(tracedir) {
+		tracedir = filepath.Join(startwd, tracedir)
+	}
+	traces, err := ioutil.ReadDir(tracedir)
+	atLeastOneTraceFound := false
+	nbErr := 0
+
+	for _, t := range traces {
+
+		// Filter out non-trace files
+		if t.IsDir() {
+			continue
+		}
+		trace := t.Name()
+		if !strings.HasSuffix(trace, ".gfxtrace") {
+			continue
+		}
+
+		atLeastOneTraceFound = true
+
+		// Run smoke tests on trace under temporary directory
+		tracewd := filepath.Join(tmpdir, trace)
+		tracepath := filepath.Join(tracedir, trace)
+		if err := os.Mkdir(tracewd, 0777); err != nil {
+			return err
+		}
+		os.Chdir(tracewd)
+		if err := testTrace(ctx, &nbErr, tracepath); err != nil {
+			return err
+		}
+		os.Chdir(startwd)
+	}
+
+	if !atLeastOneTraceFound {
+		return errors.New("No file ending with '.gfxtrace' found in trace directory")
+	}
+
+	if nbErr > 0 {
+		errStr := "error"
+		if nbErr > 1 {
+			errStr = "errors"
+		}
+		log.I(ctx, "%d %s found, see logs in %s", nbErr, errStr, tmpdir)
+	} else {
+		// Temporary directory is removed only when there are no errors
+		os.RemoveAll(tmpdir)
+	}
+	return nil
+}
+
+func testTrace(ctx context.Context, nbErr *int, tracepath string) error {
+
+	// The trace basename is used for some commands argument
+	trace := filepath.Base(tracepath)
+
+	tests := [][]string{
+		{"commands", tracepath},
+		{"create_graph_visualization", "-format", "dot", "-out", trace + ".dot", tracepath},
+		{"dump", tracepath},
+		{"dump_fbo", tracepath},
+		{"dump_pipeline", tracepath},
+		{"dump_replay", tracepath},
+		{"dump_resources", tracepath},
+		{"export_replay", tracepath},
+		{"memory", tracepath},
+		{"stats", tracepath},
+		{"trim", tracepath},
+		{"unpack", tracepath},
+	}
+
+	for _, test := range tests {
+		if err := gapit(ctx, nbErr, test...); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func gapit(ctx context.Context, nbErr *int, args ...string) error {
+	// Print command description
+	arglen := len(args)
+	argsWithoutTrace := args[:arglen-1]
+	trace := filepath.Base(args[arglen-1])
+	printCmd := "gapit " + strings.Join(argsWithoutTrace, " ") + " " + trace
+	log.I(ctx, "run: %s", printCmd)
+
+	// Execute, check error, print status
+	cmd := exec.Command("gapit", args...)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		if _, ok := err.(*exec.ExitError); ok {
+			// Here the gapit command raised an error
+			log.I(ctx, "ERROR %s", printCmd)
+			*nbErr += 1
+		} else {
+			// Here the error comes from somewhere else
+			return err
+		}
+	} else {
+		log.I(ctx, "OK %s", printCmd)
+	}
+
+	// Write output in log
+	verb := args[0]
+	if err := ioutil.WriteFile(verb+".log", output, 0666); err != nil {
+		return err
+	}
+
+	return nil
+}
