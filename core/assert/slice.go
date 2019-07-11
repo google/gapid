6b7e7b7761b7e931d5ac851b5adaf1597ecec926
Hugues Evrard <hevrard@users.noreply.github.com>
2019-07-11 15:35:57 +0100

    Fix stacktrace test by adding assert.OnSlice.EqualsOneOf() (#2896)
    EqualsOneOf() enables testing for several possibles stacktrace,
    which may arise from different optimization flags.

--- core/assert/slice.go
+++ core/assert/slice.go
@@ -53,64 +53,85 @@ func (o OnSlice) IsLength(length int) bool {
 
 // Equals asserts the array or slice matches expected.
 func (o OnSlice) Equals(expected interface{}) bool {
-	return o.slicesEqual(expected, func(a, b interface{}) bool { return a == b })
+	return o.slicesEqualTest(expected, func(a, b interface{}) bool { return a == b })
+}
+
+// Equals asserts the array or slice matches one of the expected array or slice
+func (o OnSlice) EqualsOneOf(expecteds interface{}) bool {
+	switch reflect.TypeOf(expecteds).Kind() {
+	case reflect.Slice, reflect.Array:
+		s := reflect.ValueOf(expecteds)
+		for i := 0; i < s.Len(); i++ {
+			if o.slicesEqual(s.Index(i).Interface(), func(a, b interface{}) bool { return a == b }) {
+				return true
+			}
+		}
+		// Could not find any matching slice, report failure on the last candidate
+		if s.Len() > 0 {
+			return o.slicesEqualTest(s.Index(s.Len()-1).Interface(), func(a, b interface{}) bool { return a == b })
+		}
+	}
+	o.Println("EqualsOneOf needs a non-empty slice or array (of slices or arrays) as argument")
+	return false
 }
 
 // EqualsWithComparator asserts the array or slice matches expected using a comparator function
 func (o OnSlice) EqualsWithComparator(expected interface{}, same func(a, b interface{}) bool) bool {
-	return o.slicesEqual(expected, same)
+	return o.slicesEqualTest(expected, same)
 }
 
 // DeepEquals asserts the array or slice matches expected using a deep-equal comparison.
 func (o OnSlice) DeepEquals(expected interface{}) bool {
-	return o.slicesEqual(expected, compare.DeepEqual)
+	return o.slicesEqualTest(expected, compare.DeepEqual)
 }
 
 func (o OnSlice) slicesEqual(expected interface{}, same func(a, b interface{}) bool) bool {
-	return o.Test(func() bool {
-		gs := reflect.ValueOf(o.slice)
-		glen := gs.Len()
-		es := reflect.ValueOf(expected)
-		elen := es.Len()
-		max := glen
-		if max < elen {
-			max = elen
-		}
-		equal := true
-		for i := 0; i < max; i++ {
-			switch {
-			case i >= glen:
-				// expected but not present
+	gs := reflect.ValueOf(o.slice)
+	glen := gs.Len()
+	es := reflect.ValueOf(expected)
+	elen := es.Len()
+	max := glen
+	if max < elen {
+		max = elen
+	}
+	equal := true
+	for i := 0; i < max; i++ {
+		switch {
+		case i >= glen:
+			// expected but not present
+			ev := es.Index(i)
+			o.Printf("-\t%d\t\t", i)
+			o.Printf("\t==>\t%T\t", ev.Interface())
+			o.Println(ev.Interface())
+			equal = false
+		case i >= elen:
+			// present but not expected
+			gv := gs.Index(i)
+			o.Printf("+\t%d\t%T\t", i, gv.Interface())
+			o.Print(gv.Interface())
+			o.Rawln("\t;")
+			equal = false
+		default:
+			// in both
+			ev := es.Index(i)
+			gv := gs.Index(i)
+			if same(gv.Interface(), ev.Interface()) {
+				o.Printf("\t%d\t%T\t", i, gv.Interface())
+				o.Print(gv.Interface())
+				o.Rawln("\t;")
+			} else {
 				ev := es.Index(i)
-				o.Printf("-\t%d\t\t", i)
+				o.Printf("*\t%d\t%T\t", i, gv.Interface())
+				o.Print(gv.Interface())
 				o.Printf("\t==>\t%T\t", ev.Interface())
 				o.Println(ev.Interface())
 				equal = false
-			case i >= elen:
-				// present but not expected
-				gv := gs.Index(i)
-				o.Printf("+\t%d\t%T\t", i, gv.Interface())
-				o.Print(gv.Interface())
-				o.Rawln("\t;")
-				equal = false
-			default:
-				// in both
-				ev := es.Index(i)
-				gv := gs.Index(i)
-				if same(gv.Interface(), ev.Interface()) {
-					o.Printf("\t%d\t%T\t", i, gv.Interface())
-					o.Print(gv.Interface())
-					o.Rawln("\t;")
-				} else {
-					ev := es.Index(i)
-					o.Printf("*\t%d\t%T\t", i, gv.Interface())
-					o.Print(gv.Interface())
-					o.Printf("\t==>\t%T\t", ev.Interface())
-					o.Println(ev.Interface())
-					equal = false
-				}
 			}
 		}
-		return equal
-	}())
+	}
+	return equal
+}
+
+func (o OnSlice) slicesEqualTest(expected interface{}, same func(a, b interface{}) bool) bool {
+	return o.Test(o.slicesEqual(expected, same))
 }
