8453fc854868f79929f882bca44e2463c79d81a8
Melih Yasin Yalcin <yalcinmelihyasin@users.noreply.github.com>
2019-07-11 11:13:59 +0100

    Add the test to the new kokoro test pipeline. (#2948)
    Add a Kokoro configuration to build and run tests on Linux.

--- kokoro/linux-test/test.sh
+++ kokoro/linux-test/test.sh
@@ -13,7 +13,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Test Build Script.
+# Linux Build Script.
+set -ex
 
-# Work in Progress
-echo "Placeholder for test"
+BUILD_ROOT=$PWD
+SRC=$PWD/github/gapid/
+
+# Get bazel
+curl -L -k -O -s https://github.com/bazelbuild/bazel/releases/download/0.25.1/bazel-0.25.1-installer-linux-x86_64.sh
+mkdir bazel
+bash bazel-0.25.1-installer-linux-x86_64.sh --prefix=$PWD/bazel
+
+# Get GCC 7
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get -q update
+sudo apt-get -qy install gcc-7 g++-7
+export CC=/usr/bin/gcc-7
+
+cd $SRC
+BUILD_SHA=${KOKORO_GITHUB_COMMIT:-$KOKORO_GITHUB_PULL_REQUEST_COMMIT}
+
+echo $(date): Tests started.
+$BUILD_ROOT/bazel/bin/bazel \
+    --output_base="${TMP}/bazel_out" \
+    test tests -c opt --config symbols \
+    --define GAPID_BUILD_NUMBER="$KOKORO_BUILD_NUMBER" \
+    --define GAPID_BUILD_SHA="$BUILD_SHA" \
+    --test_tag_filters=-needs_gpu
+echo $(date): Tests completed.
