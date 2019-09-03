81ce0ba0a31d6f4ce38a2f62762427c4bc1adef8
Pascal Muetschard <pmuetschard@google.com>
2019-09-03 12:34:40 -0700

    Support rendering with a bold font.

--- gapic/src/main/com/google/gapid/perfetto/canvas/Fonts.java
+++ gapic/src/main/com/google/gapid/perfetto/canvas/Fonts.java
@@ -20,6 +20,7 @@ import static com.google.gapid.perfetto.canvas.RenderContext.scale;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Device;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.FontData;
@@ -37,7 +38,7 @@ public class Fonts {
   }
 
   public static enum Style {
-    Normal;
+    Normal, Bold;
   }
 
   public static interface TextMeasurer {
@@ -45,48 +46,102 @@ public class Fonts {
   }
 
   public static class Context implements TextMeasurer {
-    private final Font font;
-    private final GC textGC;
-    private final Cache<String, Size> textExtentCache = CacheBuilder.newBuilder()
+    private final FontAndGC[] fonts = new FontAndGC[Style.values().length];
+    private final Cache<SizeCacheKey, Size> textExtentCache = CacheBuilder.newBuilder()
         .softValues()
         .recordStats()
         .build();
 
     public Context(Control owner) {
-      this.font = scaleFont(owner.getDisplay(), owner.getFont());
-      this.textGC = new GC(owner.getDisplay());
-      textGC.setFont(font);
+      this.fonts[Style.Normal.ordinal()] = FontAndGC.get(owner, SWT.NORMAL);
+      this.fonts[Style.Bold.ordinal()] = FontAndGC.get(owner, SWT.BOLD);
     }
 
     @Override
     public Size measure(Style style, String text) {
-      assert style == Style.Normal;
       try {
-        return textExtentCache.get(text, () -> Size.of(textGC.stringExtent(text), 1 / scale));
+        return textExtentCache.get(new SizeCacheKey(style, text),
+            () -> fonts[style.ordinal()].measure(text));
       } catch (ExecutionException e) {
         throw new RuntimeException(e.getCause());
       }
     }
 
     public void setFont(GC gc, Style style) {
-      assert style == Style.Normal;
-      gc.setFont(font);
+      fonts[style.ordinal()].apply(gc);
     }
 
     public void dispose() {
-      font.dispose();
-      textGC.dispose();
+      for (FontAndGC fgc : fonts) {
+        fgc.dispose();
+      }
 
       LOG.log(Level.FINE, "Text extent cache stats: {0}", textExtentCache.stats());
       textExtentCache.invalidateAll();
     }
 
-    private static Font scaleFont(Device display, Font font) {
-      FontData[] fds = font.getFontData();
-      for (FontData fd : fds) {
-        fd.setHeight(scale(fd.getHeight()));
+    private static class FontAndGC {
+      private final Font font;
+      private final GC gc;
+
+      private FontAndGC(Font font, GC gc) {
+        this.font = font;
+        this.gc = gc;
       }
-      return new Font(display, fds);
+
+      public static FontAndGC get(Control owner, int style) {
+        Font font = scaleFont(owner.getDisplay(), owner.getFont(), style);
+        GC gc = new GC(owner.getDisplay());
+        gc.setFont(font);
+        return new FontAndGC(font, gc);
+      }
+
+      public Size measure(String text) {
+        return Size.of(gc.stringExtent(text), 1 / scale);
+      }
+
+      public void apply(GC target) {
+        target.setFont(font);
+      }
+
+      public void dispose() {
+        font.dispose();
+      }
+
+      private static Font scaleFont(Device display, Font font, int style) {
+        FontData[] fds = font.getFontData();
+        for (FontData fd : fds) {
+          fd.setHeight(scale(fd.getHeight()));
+          fd.setStyle(style);
+        }
+        return new Font(display, fds);
+      }
+    }
+
+    private static class SizeCacheKey {
+      public final Style style;
+      public final String text;
+
+      public SizeCacheKey(Style style, String text) {
+        this.style = style;
+        this.text = text;
+      }
+
+      @Override
+      public int hashCode() {
+        return style.hashCode() ^ text.hashCode();
+      }
+
+      @Override
+      public boolean equals(Object obj) {
+        if (obj == this) {
+          return true;
+        } else if (!(obj instanceof SizeCacheKey)) {
+          return false;
+        }
+        SizeCacheKey o = (SizeCacheKey)obj;
+        return style == o.style && text.equals(o.text);
+      }
     }
   }
 }
