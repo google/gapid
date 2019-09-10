a57d37c8e3d579ff6586c4258376e4f1291d65d7
Pascal Muetschard <pmuetschard@google.com>
2019-09-10 15:42:25 -0700

    GPU options for the Perfetto trace config dialog.

--- gapic/src/main/com/google/gapid/perfetto/views/TraceConfigDialog.java
+++ gapic/src/main/com/google/gapid/perfetto/views/TraceConfigDialog.java
@@ -18,18 +18,26 @@ package com.google.gapid.perfetto.views;
 import static com.google.gapid.widgets.Widgets.createCheckbox;
 import static com.google.gapid.widgets.Widgets.createComposite;
 import static com.google.gapid.widgets.Widgets.createLabel;
+import static com.google.gapid.widgets.Widgets.createLink;
 import static com.google.gapid.widgets.Widgets.createSpinner;
 import static com.google.gapid.widgets.Widgets.withIndents;
 import static com.google.gapid.widgets.Widgets.withLayoutData;
+import static com.google.gapid.widgets.Widgets.withMargin;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.stream.Collectors.toSet;
 
 import com.google.gapid.models.Models;
 import com.google.gapid.models.Settings;
+import com.google.gapid.proto.device.Device;
+import com.google.gapid.proto.device.GpuProfiling;
 import com.google.gapid.util.Messages;
 import com.google.gapid.widgets.DialogBase;
 import com.google.gapid.widgets.Theme;
 import com.google.gapid.widgets.Widgets;
 
+import org.eclipse.jface.window.Window;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
@@ -38,8 +46,12 @@ import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Spinner;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
 
 import java.util.Arrays;
+import java.util.Set;
 
 import perfetto.protos.PerfettoConfig;
 
@@ -80,21 +92,34 @@ public class TraceConfigDialog extends DialogBase {
   };
 
   private final Settings settings;
+  private final Device.PerfettoCapability caps;
   private InputArea input;
 
-  public TraceConfigDialog(Shell shell, Settings settings, Theme theme) {
+  public TraceConfigDialog(
+      Shell shell, Settings settings, Theme theme, Device.PerfettoCapability caps) {
     super(shell, theme);
     this.settings = settings;
+    this.caps = caps;
   }
 
-  public static void showPerfettoConfigDialog(Shell shell, Models models, Widgets widgets) {
-    new TraceConfigDialog(shell, models.settings, widgets.theme).open();
+  public static void showPerfettoConfigDialog(
+      Shell shell, Models models, Widgets widgets, Device.PerfettoCapability caps) {
+    new TraceConfigDialog(shell, models.settings, widgets.theme, caps).open();
   }
 
-  public static String getConfigSummary(Settings settings) {
+  public static String getConfigSummary(Settings settings, Device.PerfettoCapability caps) {
     StringBuilder sb = new StringBuilder();
     if (settings.perfettoCpu) {
       sb.append("CPU");
+    }
+    Device.GPUProfiling gpuCaps = caps.getGpuProfiling();
+    if (gpuCaps.getHasRenderStage() || gpuCaps.getGpuCounterDescriptor().getSpecsCount() > 0) {
+      if (settings.perfettoGpu) {
+        if (sb.length() > 0) {
+          sb.append(", ");
+        }
+        sb.append("GPU");
+      }
     }
     if (settings.perfettoMem) {
       if (sb.length() > 0) {
@@ -105,7 +130,8 @@ public class TraceConfigDialog extends DialogBase {
     return sb.toString();
   }
 
-  public static PerfettoConfig.TraceConfig.Builder getConfig(Settings settings) {
+  public static PerfettoConfig.TraceConfig.Builder getConfig(
+      Settings settings, Device.PerfettoCapability caps) {
     PerfettoConfig.TraceConfig.Builder config = PerfettoConfig.TraceConfig.newBuilder()
         .addBuffers(PerfettoConfig.TraceConfig.BufferConfig.newBuilder()
             .setSizeKb(BUFFER_SIZE));
@@ -135,6 +161,27 @@ public class TraceConfigDialog extends DialogBase {
       }
     }
 
+    Device.GPUProfiling gpuCaps = caps.getGpuProfiling();
+    if (gpuCaps.getHasRenderStage() || gpuCaps.getGpuCounterDescriptor().getSpecsCount() > 0) {
+      if (settings.perfettoGpu) {
+        if (settings.perfettoGpuSlices) {
+          config.addDataSourcesBuilder()
+              .getConfigBuilder()
+                  .setName("gpu.timing");
+        }
+        if (settings.perfettoGpuCounters && settings.perfettoGpuCounterIds.length > 0) {
+          PerfettoConfig.GpuCounterConfig.Builder counters = config.addDataSourcesBuilder()
+              .getConfigBuilder()
+                  .setName("gpu.counters")
+                  .getGpuCounterConfigBuilder()
+                      .setCounterPeriodNs(MILLISECONDS.toNanos(settings.perfettoGpuCounterRate));
+          for (int counter : settings.perfettoGpuCounterIds) {
+            counters.addCounterIds(counter);
+          }
+        }
+      }
+    }
+
     if (settings.perfettoMem) {
       config.addDataSourcesBuilder()
           .getConfigBuilder()
@@ -155,7 +202,8 @@ public class TraceConfigDialog extends DialogBase {
   @Override
   protected Control createDialogArea(Composite parent) {
     Composite area = (Composite)super.createDialogArea(parent);
-    input = withLayoutData(new InputArea(area, settings), new GridData(GridData.FILL_BOTH));
+    input = withLayoutData(
+        new InputArea(area, settings, theme, caps), new GridData(GridData.FILL_BOTH));
     return area;
   }
 
@@ -173,31 +221,94 @@ public class TraceConfigDialog extends DialogBase {
     private final Button cpuChain;
     private final Button cpuSlices;
 
+    private final Button gpu;
+    private final Button gpuSlices;
+    private final Button gpuCounters;
+    private final Label[] gpuCountersLabels;
+    private final Button gpuCountersSelect;
+    private final Spinner gpuCountersRate;
+
     private final Button mem;
     private final Label[] memLabels;
     private final Spinner memRate;
 
-    public InputArea(Composite parent, Settings settings) {
+    public InputArea(
+        Composite parent, Settings settings, Theme theme, Device.PerfettoCapability caps) {
       super(parent, SWT.NONE);
       setLayout(new GridLayout(1, false));
 
       cpu = createCheckbox(this, "CPU", settings.perfettoCpu, e -> updateCpu());
-      Composite cpuGroup = withLayoutData(createComposite(this, new GridLayout(1, false)),
+      Composite cpuGroup = withLayoutData(
+          createComposite(this, withMargin(new GridLayout(1, false), 5, 0)),
           withIndents(new GridData(), GROUP_INDENT, 0));
       cpuFreq = createCheckbox(cpuGroup, "Frequency and idle states", settings.perfettoCpuFreq);
       cpuChain = createCheckbox(cpuGroup, "Scheduling chains / latency", settings.perfettoCpuChain);
       cpuSlices = createCheckbox(cpuGroup, "Thread slices", settings.perfettoCpuSlices);
       addSeparator();
 
+      Device.GPUProfiling gpuCaps = caps.getGpuProfiling();
+      if (gpuCaps.getHasRenderStage() || gpuCaps.getGpuCounterDescriptor().getSpecsCount() > 0) {
+        gpu = createCheckbox(this, "GPU", settings.perfettoGpu, e -> updateGpu());
+        Composite gpuGroup = withLayoutData(
+            createComposite(this, withMargin(new GridLayout(1, false), 5, 0)),
+            withIndents(new GridData(), GROUP_INDENT, 0));
+        if (gpuCaps.getHasRenderStage()) {
+          gpuSlices = createCheckbox(gpuGroup, "Renderstage slices", settings.perfettoGpuSlices);
+        } else {
+          gpuSlices = null;
+        }
+
+        if (gpuCaps.getGpuCounterDescriptor().getSpecsCount() > 0) {
+          gpuCounters = createCheckbox(
+              gpuGroup, "Counters", settings.perfettoGpuCounters, e -> updateGpu());
+          Composite counterGroup = withLayoutData(
+              createComposite(gpuGroup, withMargin(new GridLayout(3, false), 5, 0)),
+              withIndents(new GridData(), GROUP_INDENT, 0));
+          gpuCountersLabels = new Label[3];
+          gpuCountersLabels[1] = createLabel(counterGroup, "Poll Rate:");
+          gpuCountersRate = createSpinner(counterGroup, settings.perfettoGpuCounterRate, 1, 1000);
+          gpuCountersLabels[2] = createLabel(counterGroup, "ms");
+
+          gpuCountersLabels[0] = createLabel(
+              counterGroup, settings.perfettoGpuCounterIds.length + " selected");
+          gpuCountersSelect = Widgets.createButton(counterGroup, "Select", e -> {
+            GpuCountersDialog dialog =
+                new GpuCountersDialog(getShell(), theme, caps, settings.perfettoGpuCounterIds);
+            if (dialog.open() == Window.OK) {
+              settings.perfettoGpuCounterIds = dialog.getSelectedIds();
+              gpuCounters.setSelection(settings.perfettoGpuCounterIds.length != 0);
+              gpuCountersLabels[0].setText(settings.perfettoGpuCounterIds.length + " selected");
+              gpuCountersLabels[0].requestLayout();
+              updateGpu();
+            }
+          });
+        } else {
+          gpuCounters = null;
+          gpuCountersLabels = null;
+          gpuCountersRate = null;
+          gpuCountersSelect = null;
+        }
+        addSeparator();
+      } else {
+        gpu = null;
+        gpuSlices = null;
+        gpuCounters = null;
+        gpuCountersLabels = null;
+        gpuCountersRate = null;
+        gpuCountersSelect = null;
+      }
+
       mem = createCheckbox(this, "Memory", settings.perfettoMem, e -> updateMem());
       memLabels = new Label[2];
-      Composite memGroup = withLayoutData(createComposite(this, new GridLayout(3, false)),
+      Composite memGroup = withLayoutData(
+          createComposite(this, withMargin(new GridLayout(3, false), 5, 0)),
           withIndents(new GridData(), GROUP_INDENT, 0));
       memLabels[0] = createLabel(memGroup, "Poll Rate:");
       memRate = createSpinner(memGroup, settings.perfettoMemRate, 1, 1000);
       memLabels[1] = createLabel(memGroup, "ms");
 
       updateCpu();
+      updateGpu();
       updateMem();
     }
 
@@ -206,6 +317,17 @@ public class TraceConfigDialog extends DialogBase {
       settings.perfettoCpuChain = cpuChain.getSelection();
       settings.perfettoCpuFreq = cpuFreq.getSelection();
       settings.perfettoCpuSlices = cpuSlices.getSelection();
+
+      if (gpu != null) {
+        settings.perfettoGpu = gpu.getSelection();
+      }
+      if (gpuSlices != null) {
+        settings.perfettoGpuSlices = gpuSlices.getSelection();
+      }
+      if (gpuCounters != null) {
+        settings.perfettoGpuCounters = gpuCounters.getSelection();
+        settings.perfettoGpuCounterRate = gpuCountersRate.getSelection();
+      }
 
       settings.perfettoMem = mem.getSelection();
       settings.perfettoMemRate = memRate.getSelection();
@@ -223,11 +345,98 @@ public class TraceConfigDialog extends DialogBase {
       cpuSlices.setEnabled(enabled);
     }
 
+    private void updateGpu() {
+      if (gpu == null) {
+        return;
+      }
+
+      boolean enabled = gpu.getSelection();
+      if (gpuSlices != null) {
+        gpuSlices.setEnabled(enabled);
+      }
+      if (gpuCounters != null) {
+        gpuCounters.setEnabled(enabled);
+        boolean countersEnabled = enabled && gpuCounters.getSelection();
+        gpuCountersRate.setEnabled(countersEnabled);
+        gpuCountersSelect.setEnabled(countersEnabled);
+        for (Label label : gpuCountersLabels) {
+          label.setEnabled(countersEnabled);
+        }
+      }
+    }
+
     private void updateMem() {
       boolean enabled = mem.getSelection();
       memRate.setEnabled(enabled);
       for (Label label : memLabels) {
         label.setEnabled(enabled);
+      }
+    }
+
+    private static class GpuCountersDialog extends DialogBase {
+      private final Device.PerfettoCapability caps;
+      private final Set<Integer> currentIds;
+
+      private Table table;
+      private int[] selectedIds;
+
+      public GpuCountersDialog(
+          Shell shell, Theme theme, Device.PerfettoCapability caps, int[] currentIds) {
+        super(shell, theme);
+        this.caps = caps;
+        this.currentIds = Arrays.stream(currentIds).boxed().collect(toSet());
+      }
+
+      public int[] getSelectedIds() {
+        return selectedIds;
+      }
+
+      @Override
+      public String getTitle() {
+        return Messages.CAPTURE_TRACE_PERFETTO;
+      }
+
+      @Override
+      protected Control createDialogArea(Composite parent) {
+        Composite area = (Composite)super.createDialogArea(parent);
+        table = withLayoutData(new Table(area, SWT.CHECK), new GridData(GridData.FILL_BOTH));
+        table.setHeaderVisible(true);
+        table.setLinesVisible(true);
+        new TableColumn(table, SWT.NONE).setText("Name");
+        new TableColumn(table, SWT.NONE).setText("Description");
+        for (GpuProfiling.GpuCounterDescriptor.GpuCounterSpec counter :
+            caps.getGpuProfiling().getGpuCounterDescriptor().getSpecsList()) {
+          TableItem item = new TableItem(table, SWT.NONE);
+          item.setText(new String[] { counter.getName(), counter.getDescription() });
+          item.setData(counter);
+          if (currentIds.contains(counter.getCounterId())) {
+            item.setChecked(true);
+          }
+        }
+        table.getColumn(0).pack();
+        table.getColumn(1).pack();
+        createLink(area, "Select <a>none</a> | <a>all</a>", e -> {
+          boolean checked = "all".equals(e.text);
+          for (TableItem item : table.getItems()) {
+            item.setChecked(checked);
+          }
+        });
+        return area;
+      }
+
+      @Override
+      protected Point getInitialSize() {
+        return new Point(convertHorizontalDLUsToPixels(450), convertVerticalDLUsToPixels(300));
+      }
+
+      @Override
+      protected void okPressed() {
+        selectedIds = Arrays.stream(table.getItems())
+            .filter(item -> item.getChecked())
+            .map(item -> (GpuProfiling.GpuCounterDescriptor.GpuCounterSpec)item.getData())
+            .mapToInt(GpuProfiling.GpuCounterDescriptor.GpuCounterSpec::getCounterId)
+            .toArray();
+        super.okPressed();
       }
     }
   }
