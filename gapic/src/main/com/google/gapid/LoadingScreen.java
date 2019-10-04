google/gapid

commit 6c08bac0e70917ec3a9d96c6bd8d4c16e7640d59

Updates to the welcome screen's options.

gapic/src/main/com/google/gapid/LoadingScreen.java
@@ -18,15 +18,18 @@ package com.google.gapid;
 import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.gapid.util.GapidVersion.GAPID_VERSION;
 import static com.google.gapid.util.GeoUtils.bottomLeft;
-import static com.google.gapid.views.AboutDialog.showHelp;
 import static com.google.gapid.views.TracerDialog.showOpenTraceDialog;
 import static com.google.gapid.views.TracerDialog.showTracingDialog;
 import static com.google.gapid.widgets.Widgets.createComposite;
 import static com.google.gapid.widgets.Widgets.createLabel;
-import static com.google.gapid.widgets.Widgets.createLink;
 import static com.google.gapid.widgets.Widgets.createMenuItem;
+import static com.google.gapid.widgets.Widgets.filling;
+import static com.google.gapid.widgets.Widgets.recursiveAddListener;
 import static com.google.gapid.widgets.Widgets.scheduleIfNotDisposed;
-import static com.google.gapid.widgets.Widgets.withMargin;
+import static com.google.gapid.widgets.Widgets.withIndents;
+import static com.google.gapid.widgets.Widgets.withLayoutData;
+import static com.google.gapid.widgets.Widgets.withMarginAndSpacing;
+import static com.google.gapid.widgets.Widgets.withSpacing;
 
 import com.google.gapid.models.Analytics.View;
 import com.google.gapid.models.Models;
@@ -34,16 +37,22 @@ import com.google.gapid.proto.service.Service.ClientAction;
 import com.google.gapid.server.Client;
 import com.google.gapid.util.Messages;
 import com.google.gapid.util.OS;
+import com.google.gapid.views.AboutDialog;
 import com.google.gapid.widgets.CenteringLayout;
 import com.google.gapid.widgets.Theme;
 import com.google.gapid.widgets.Widgets;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Link;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Menu;
 
 import java.io.File;
@@ -83,8 +92,8 @@ public class LoadingScreen extends Composite {
     statusLabel = createLabel(container, "Starting up...");
     statusLabel.setLayoutData(new GridData(SWT.CENTER, SWT.TOP, true, false));
 
-    optionsContainer = createComposite(container, withMargin(new GridLayout(3, false), 15, 5));
-    optionsContainer.setLayoutData(new GridData(SWT.CENTER, SWT.TOP, true, false));
+    optionsContainer = createComposite(
+        container, withSpacing(filling(new RowLayout(SWT.VERTICAL), true, true), 8));
     createOptions();
   }
 
@@ -112,24 +121,14 @@ public class LoadingScreen extends Composite {
    * Initialize the links for layout settings. Hide them until server set up.
    */
   private void createOptions() {
-    createLabel(optionsContainer, "", theme.add());
-    createLink(optionsContainer, "<a>Capture a new trace</a>", e -> {
-      showTracingDialog(checkNotNull(client), getShell(), checkNotNull(models), checkNotNull(widgets));
+    OptionBar.withShortcut(theme, optionsContainer, theme.add(), "Capture a new trace", "T", e -> {
+      showTracingDialog(
+          checkNotNull(client), getShell(), checkNotNull(models), checkNotNull(widgets));
     });
-    Label captureHint = createLabel(optionsContainer, (OS.isMac ? "\u2318" : "Ctrl") + " + T");
-    captureHint.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false));
-    captureHint.setForeground(theme.shortcutKeyHintColor());
-
-    createLabel(optionsContainer, "", theme.open());
-    createLink(optionsContainer, "<a>Open an existing trace</a>", e -> {
-      showOpenTraceDialog(getShell(), checkNotNull(this.models));
+    OptionBar.withShortcut(theme, optionsContainer, theme.open(), "Open an existing trace", "O", e -> {
+      showOpenTraceDialog(getShell(), checkNotNull(models));
     });
-    Label openHint = createLabel(optionsContainer, (OS.isMac ? "\u2318" : "Ctrl") + " + O");
-    openHint.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false));
-    openHint.setForeground(theme.shortcutKeyHintColor());
-
-    createLabel(optionsContainer, "", theme.recent());
-    recentLink = createLink(optionsContainer, "<a>Open recent traces</a>", e -> {
+    OptionBar.withDropDown(theme, optionsContainer, theme.recent(), "Open recent trace", e -> {
       Menu popup = new Menu(optionsContainer);
       Arrays.stream(checkNotNull(models).settings.recentFiles)
           .map(f -> truncate(f))
@@ -138,13 +137,13 @@ public class LoadingScreen extends Composite {
               checkNotNull(models).capture.loadCapture(new File(file));
           }));
       popup.addListener(SWT.Hide, ev -> scheduleIfNotDisposed(popup, popup::dispose));
-      popup.setLocation(optionsContainer.toDisplay(bottomLeft(((Link)e.widget).getBounds())));
+      popup.setLocation(
+          optionsContainer.toDisplay(bottomLeft((((Control)e.widget).getParent().getBounds()))));
       popup.setVisible(true);
     });
-    recentLink.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false, 2, 1));
-
-    createLabel(optionsContainer, "", theme.help());
-    createLink(optionsContainer, "<a>Help</a>", e -> showHelp(models.analytics));
+    OptionBar.simple(theme, optionsContainer, theme.help(), "Help", e -> {
+      AboutDialog.showHelp(checkNotNull(models).analytics);
+    });
 
     optionsContainer.setVisible(false);
   }
@@ -162,5 +161,51 @@ public class LoadingScreen extends Composite {
       return "..." + file.substring(file.length() - MAX_FILE_NAME + 3);
     }
     return "..." + file;
+  }
+
+  private static class OptionBar extends Composite {
+    private OptionBar(
+        Composite parent, Image icon, String label, Image dropDown, String shortcut, Color hover) {
+      super(parent, SWT.NONE);
+      setLayout(withMarginAndSpacing(new GridLayout(4, false), 10, 0, 0, 0));
+
+      createLabel(this, "", icon);
+      withLayoutData(createLabel(this, label),
+          withIndents(new GridData(SWT.LEFT, SWT.CENTER, false, false), 10, 0));
+      if (dropDown != null) {
+        createLabel(this, "", dropDown);
+      }
+      if (shortcut != null) {
+        withLayoutData(createLabel(this, (OS.isMac ? "\u2318 + " : "Ctrl + ") + shortcut),
+            withIndents(new GridData(SWT.RIGHT, SWT.CENTER, true, false), 40, 0));
+      }
+
+      setCursor(getDisplay().getSystemCursor(SWT.CURSOR_HAND));
+      setBackgroundMode(SWT.INHERIT_FORCE);
+      recursiveAddListener(this, SWT.MouseEnter, e -> setBackground(hover));
+      recursiveAddListener(this, SWT.MouseExit, e -> setBackground(null));
+    }
+
+    public static OptionBar simple(
+        Theme theme, Composite parent, Image icon, String label, Listener onClick) {
+      OptionBar bar = new OptionBar(parent, icon, label, null, null, theme.welcomeHoverColor());
+      recursiveAddListener(bar, SWT.MouseUp, onClick);
+      return bar;
+    }
+
+    public static OptionBar withShortcut(Theme theme,
+        Composite parent, Image icon, String label, String shortcut, Listener onClick) {
+      OptionBar bar = new OptionBar(parent, icon, label, null, shortcut, theme.welcomeHoverColor());
+      recursiveAddListener(bar, SWT.MouseUp, onClick);
+      return bar;
+    }
+
+    public static OptionBar withDropDown(
+        Theme theme, Composite parent, Image icon, String label, Listener onClick) {
+      OptionBar bar = new OptionBar(
+          parent, icon, label, theme.arrowDropDownLight(), null, theme.welcomeHoverColor());
+      recursiveAddListener(bar, SWT.MouseUp, onClick);
+      return bar;
+    }
   }
 }

gapic/src/main/com/google/gapid/widgets/Theme.java
@@ -137,7 +137,7 @@ public interface Theme {
 
   // About & Welcome dialog text colors
   @RGB(argb = 0xffa9a9a9) public Color welcomeVersionColor();
-  @RGB(argb = 0xffa9a9a9) public Color shortcutKeyHintColor();
+  @RGB(argb = 0xffbcdbff) public Color welcomeHoverColor();
 
   // Logging view colors by log level.
   @RGB(argb = 0xbb000000) public Color logVerboseForeground();

gapic/src/main/com/google/gapid/widgets/Widgets.java
@@ -745,6 +745,20 @@ public class Widgets {
     return composite;
   }
 
+  /**
+   * Recursively adds the given listener to the composite and all its children.
+   */
+  public static void recursiveAddListener(Composite composite, int eventType, Listener listener) {
+    composite.addListener(eventType, listener);
+    for (Control child : composite.getChildren()) {
+      if (child instanceof Composite) {
+        recursiveAddListener((Composite)child, eventType, listener);
+      } else {
+        child.addListener(eventType, listener);
+      }
+    }
+  }
+
   public static <C extends Control> C withLayoutData(C control, Object layoutData) {
     control.setLayoutData(layoutData);
     return control;
@@ -782,6 +796,15 @@ public class Widgets {
     return layout;
   }
 
+  public static GridLayout withMarginAndSpacing(GridLayout layout,
+      int marginWidth, int marginHeight, int horizontalSpacing, int verticalSpacing) {
+    layout.marginWidth = marginWidth;
+    layout.marginHeight = marginHeight;
+    layout.horizontalSpacing = horizontalSpacing;
+    layout.verticalSpacing = verticalSpacing;
+    return layout;
+  }
+
   public static GridLayout withSpacing(GridLayout layout, int horizontalSpacing, int verticalSpacing) {
     layout.horizontalSpacing = horizontalSpacing;
     layout.verticalSpacing = verticalSpacing;
@@ -791,6 +814,11 @@ public class Widgets {
   public static RowLayout withMargin(RowLayout layout, int marginWidth, int marginHeight) {
     layout.marginWidth = marginWidth;
     layout.marginHeight = marginHeight;
+    return layout;
+  }
+
+  public static RowLayout withSpacing(RowLayout layout, int spacing) {
+    layout.spacing = spacing;
     return layout;
   }
 

commit b51c3e8366a45f65409e56c4328bcddbeafff3b4

Fix open-recent bug introduced in #3214.

gapic/src/main/com/google/gapid/LoadingScreen.java
@@ -56,7 +56,6 @@ import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Menu;
 
 import java.io.File;
-import java.util.Arrays;
 
 /**
  * The loading screen is a minimal view shown while the UI is loading, looking for gapis, etc.
@@ -130,12 +129,12 @@ public class LoadingScreen extends Composite {
     });
     OptionBar.withDropDown(theme, optionsContainer, theme.recent(), "Open recent trace", e -> {
       Menu popup = new Menu(optionsContainer);
-      Arrays.stream(checkNotNull(models).settings.recentFiles)
-          .map(f -> truncate(f))
-          .forEachOrdered(file -> createMenuItem(popup, file, 0, ev -> {
-              checkNotNull(models).analytics.postInteraction(View.Welcome, ClientAction.OpenRecent);
-              checkNotNull(models).capture.loadCapture(new File(file));
-          }));
+      for (String file : checkNotNull(models).settings.recentFiles) {
+        createMenuItem(popup, truncate(file), 0, ev -> {
+          checkNotNull(models).analytics.postInteraction(View.Welcome, ClientAction.OpenRecent);
+          checkNotNull(models).capture.loadCapture(new File(file));
+        });
+      }
       popup.addListener(SWT.Hide, ev -> scheduleIfNotDisposed(popup, popup::dispose));
       popup.setLocation(
           optionsContainer.toDisplay(bottomLeft((((Control)e.widget).getParent().getBounds()))));