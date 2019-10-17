google/gapid

commit 1116a53332e79d86399b8a84a0a1e02d7136bd95

Check for dev releases tagged <major>.<minor>.<point>-dev-<dev> (#3104)
Also add the relevant option in the Settings UI, fix the default state
of Settings dialog checkboxes, and reset update timer whenever
Settings are saved.

gapic/src/main/com/google/gapid/views/SettingsDialog.java
@@ -108,6 +108,7 @@ public class SettingsDialog extends DialogBase {
     private final Button allowAnalytics;
     private final Button allowCrashReports;
     private final Button allowUpdateChecks;
+    private final Button includePrerelease;
 
     public SettingsFormBase(Models models, Composite parent) {
       this(models, parent, 5, 5);
@@ -129,14 +130,17 @@ public class SettingsDialog extends DialogBase {
       beforeAnalytics();
 
       allowAnalytics = withLayoutData(
-          createCheckbox(this, Messages.ANALYTICS_OPTION, true),
+          createCheckbox(this, Messages.ANALYTICS_OPTION, models.settings.analyticsEnabled()),
           withSpans(new GridData(SWT.LEFT, SWT.TOP, false, false), 2, 1));
       allowCrashReports = withLayoutData(
-          createCheckbox(this, Messages.CRASH_REPORTING_OPTION, true),
+          createCheckbox(this, Messages.CRASH_REPORTING_OPTION, models.settings.reportCrashes),
           withSpans(new GridData(SWT.LEFT, SWT.TOP, false, false), 2, 1));
       allowUpdateChecks = withLayoutData(
-          createCheckbox(this, Messages.UPDATE_CHECK_OPTION, true),
+          createCheckbox(this, Messages.UPDATE_CHECK_OPTION, models.settings.autoCheckForUpdates),
           withSpans(new GridData(SWT.LEFT, SWT.TOP, false, false), 2, 1));
+      includePrerelease = withLayoutData(
+          createCheckbox(this, Messages.UPDATE_CHECK_PRERELEASE_OPTION, models.settings.includePrereleases),
+          withIndents(new GridData(SWT.LEFT, SWT.TOP, false, false), 20, 0));
       Label adbWarning = withLayoutData(createLabel(this, ""),
           withSpans(new GridData(SWT.FILL, SWT.FILL, true, false), 2, 1));
       adbWarning.setForeground(getDisplay().getSystemColor(SWT.COLOR_DARK_RED));
@@ -156,6 +160,13 @@ public class SettingsDialog extends DialogBase {
       };
       adbPath.addBoxListener(SWT.Modify, adbListener);
       adbListener.handleEvent(null);
+
+      allowUpdateChecks.addListener(SWT.Selection, e -> {
+        if (!allowUpdateChecks.getSelection()) {
+          includePrerelease.setSelection(false);
+        }
+        includePrerelease.setEnabled(allowUpdateChecks.getSelection());
+      });
     }
 
     protected void beforeAnalytics() {
@@ -167,6 +178,9 @@ public class SettingsDialog extends DialogBase {
       models.settings.setAnalyticsEnabled(allowAnalytics.getSelection());
       models.settings.reportCrashes = allowCrashReports.getSelection();
       models.settings.autoCheckForUpdates = allowUpdateChecks.getSelection();
+      models.settings.includePrereleases = includePrerelease.getSelection();
+      // When settings are saved, reset the update timer, to force update check on next start
+      models.settings.lastCheckForUpdates = 0;
       models.settings.save();
       models.analytics.updateSettings();
     }

core/app/run.go
@@ -97,6 +97,11 @@ func (v VersionSpec) GreaterThan(o VersionSpec) bool {
 	}
 }
 
+// Equal returns true if v is equal to o, ignoring the Build field.
+func (v VersionSpec) Equal(o VersionSpec) bool {
+	return (v.Major == o.Major) && (v.Minor == o.Minor) && (v.Point == o.Point)
+}
+
 func (v VersionSpec) String() string {
 	return fmt.Sprintf("%v", v)
 }

gapic/src/main/com/google/gapid/models/Settings.java
@@ -86,6 +86,7 @@ public class Settings {
   public String[] recentFiles = new String[0];
   public String adb = "";
   public boolean autoCheckForUpdates = true;
+  public boolean includePrereleases = false;
   public boolean updateAvailable = false;
   public long lastCheckForUpdates = 0; // milliseconds since midnight, January 1, 1970 UTC.
   public String analyticsClientId = ""; // Empty means do not track.
@@ -250,6 +251,7 @@ public class Settings {
     recentFiles = getStringList(properties, "open.recent", recentFiles);
     adb = tryFindAdb(properties.getProperty("adb.path", ""));
     autoCheckForUpdates = getBoolean(properties, "updates.autoCheck", autoCheckForUpdates);
+    includePrereleases = getBoolean(properties, "updates.includePrereleases", includePrereleases);
     lastCheckForUpdates = getLong(properties, "updates.lastCheck", 0);
     updateAvailable = getBoolean(properties, "updates.available", updateAvailable);
     analyticsClientId = properties.getProperty("analytics.clientId", "");
@@ -306,6 +308,7 @@ public class Settings {
     setStringList(properties, "open.recent", recentFiles);
     properties.setProperty("adb.path", adb);
     properties.setProperty("updates.autoCheck", Boolean.toString(autoCheckForUpdates));
+    properties.setProperty("updates.includePrereleases", Boolean.toString(includePrereleases));
     properties.setProperty("updates.lastCheck", Long.toString(lastCheckForUpdates));
     properties.setProperty("updates.available", Boolean.toString(updateAvailable));
     properties.setProperty("analytics.clientId", analyticsClientId);

gapic/src/main/com/google/gapid/util/Messages.java
@@ -63,7 +63,8 @@ public interface Messages {
       "Help improve GAPID by sending usage statistics to Google";
   public static final String CRASH_REPORTING_OPTION =
       "Help GAPID identify issues by sending crash reports to Google";
-  public static final String UPDATE_CHECK_OPTION = "Automatically check for GAPID updates";
+  public static final String UPDATE_CHECK_OPTION = "Automatically check for GAPID updates (please restart GAPID to force an update check)";
+  public static final String UPDATE_CHECK_PRERELEASE_OPTION = "Include developer preview releases";
   public static final String PRIVACY_POLICY =
       "Google's <a href=\"TOS\">APIs Terms of Service</a> and <a href=\"PP\">Privacy Policy</a>" +
       " govern your use of this application.";

gapic/src/main/com/google/gapid/util/UpdateWatcher.java
@@ -33,7 +33,6 @@ public class UpdateWatcher {
   private static final Logger LOG = Logger.getLogger(UpdateWatcher.class.getName());
 
   private static final long CHECK_INTERVAL_MS = TimeUnit.HOURS.toMillis(8);
-  private static final boolean INCLUDE_PRE_RELEASES = false;
 
   private final Settings settings;
   private final Client client;
@@ -65,7 +64,7 @@ public class UpdateWatcher {
 
   private void doCheck() {
     if (settings.autoCheckForUpdates) {
-      ListenableFuture<Release> future = client.checkForUpdates(INCLUDE_PRE_RELEASES);
+      ListenableFuture<Release> future = client.checkForUpdates(settings.includePrereleases);
       settings.updateAvailable = false;
       try {
         Release release = future.get();

gapis/server/server.go
@@ -129,13 +129,29 @@ func (s *server) CheckForUpdates(ctx context.Context, includePrereleases bool) (
 	}
 	var mostRecent *service.Release
 	mostRecentVersion := app.Version
+	mostRecentDevVersion := -1
+
 	for _, release := range releases {
 		if !includePrereleases && release.GetPrerelease() {
 			continue
 		}
+
+		// tagName is either:
+		// Regular release: v<major>.<minor>.<point>
+		// Dev pre-release: v<major>.<minor>.<point>-dev-<dev>
 		var version app.VersionSpec
-		fmt.Sscanf(release.GetTagName(), "v%d.%d.%d", &version.Major, &version.Minor, &version.Point)
-		if version.GreaterThan(mostRecentVersion) {
+		tagName := release.GetTagName()
+		devVersion := -1
+		numFields, err := fmt.Sscanf(tagName, "v%d.%d.%d-dev-%d", &version.Major, &version.Minor, &version.Point, &devVersion)
+		if err != nil && numFields < 3 {
+			// some non-release tags have other format, e.g. libinterceptor-v1.0
+			log.I(ctx, "Ignoring tag %s", tagName)
+			continue
+		}
+
+		// dev-releases are previews of the next release, so e.g. 1.2.3 is more recent than 1.2.3-dev-456
+		if version.GreaterThan(mostRecentVersion) ||
+			(version.Equal(mostRecentVersion) && mostRecentDevVersion != -1 && mostRecentDevVersion < devVersion) {
 			mostRecent = &service.Release{
 				Name:         release.GetName(),
 				VersionMajor: uint32(version.Major),
@@ -145,6 +161,7 @@ func (s *server) CheckForUpdates(ctx context.Context, includePrereleases bool) (
 				BrowserUrl:   release.GetHTMLURL(),
 			}
 			mostRecentVersion = version
+			mostRecentDevVersion = devVersion
 		}
 	}
 	if mostRecent == nil {

commit a88534961f3ca6dc915a65d374fba3a893087b85

Fix the settings dialog layout broken in #3104.

gapic/src/main/com/google/gapid/views/SettingsDialog.java
@@ -140,14 +140,13 @@ public class SettingsDialog extends DialogBase {
           withSpans(new GridData(SWT.LEFT, SWT.TOP, false, false), 2, 1));
       includePrerelease = withLayoutData(
           createCheckbox(this, Messages.UPDATE_CHECK_PRERELEASE_OPTION, models.settings.includePrereleases),
-          withIndents(new GridData(SWT.LEFT, SWT.TOP, false, false), 20, 0));
+          withSpans(withIndents(new GridData(SWT.LEFT, SWT.TOP, false, false), 20, 0), 2, 1));
       Label adbWarning = withLayoutData(createLabel(this, ""),
           withSpans(new GridData(SWT.FILL, SWT.FILL, true, false), 2, 1));
       adbWarning.setForeground(getDisplay().getSystemColor(SWT.COLOR_DARK_RED));
       withLayoutData(
           createLink(this, Messages.PRIVACY_POLICY, WelcomeDialog::showPolicy),
-          withIndents(
-              withSpans(new GridData(SWT.LEFT, SWT.TOP, false, false), 2, 1), 0, 20));
+          withSpans(withIndents(new GridData(SWT.LEFT, SWT.TOP, false, false), 0, 20), 2, 1));
 
       Listener adbListener = e -> {
         String error = Settings.checkAdbIsValid(adbPath.getText().trim());