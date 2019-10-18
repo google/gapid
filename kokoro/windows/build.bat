8730a3691b339cb2faa70f6365b43a78d55ba575
Hugues Evrard <hevrard@google.com>
2019-10-18 12:17:10 +0100

    windows bazel 0.28.1

--- kokoro/windows/build.bat
+++ kokoro/windows/build.bat
@@ -45,7 +45,7 @@ set PATH=c:\tools\msys64\mingw64\bin;c:\tools\msys64\usr\bin;%PATH%
 set BAZEL_SH=C:\tools\msys64\usr\bin\bash.exe
 
 REM Install Bazel.
-set BAZEL_VERSION=0.29.1
+set BAZEL_VERSION=0.28.1
 wget -q https://github.com/bazelbuild/bazel/releases/download/%BAZEL_VERSION%/bazel-%BAZEL_VERSION%-windows-x86_64.zip
 unzip -q bazel-%BAZEL_VERSION%-windows-x86_64.zip
 set PATH=C:\python27;%PATH%
