cccf8d12fb1b5bbb1bb131d62a869688032e301d
Hugues Evrard <hevrard@users.noreply.github.com>
2019-11-18 15:47:05 -0800

    Fix MacOS builds on Kokoro, after Kokoro update to Mojave (#3415)

--- kokoro/macos/package.sh
+++ kokoro/macos/package.sh
@@ -70,7 +70,8 @@ iconutil -c icns -o GAPID.app/Contents/Resources/GAPID.icns GAPID.iconset
 pip install --upgrade --user dmgbuild pyobjc-framework-Quartz
 cp "$SRC"/background*.png .
 cp "$SRC/dmg-settings.py" .
-~/Library/Python/2.7/bin/dmgbuild -s dmg-settings.py GAPID gapid-$VERSION-macos.dmg
+# Path to dmgbuild must match where pip installs it
+~/Library/Python/3.7/bin/dmgbuild -s dmg-settings.py GAPID gapid-$VERSION-macos.dmg
 
 # Copy the symbol file to the output.
 [ -f "$BIN/cmd/gapir/cc/gapir.sym" ] && cp "$BIN/cmd/gapir/cc/gapir.sym" gapir-$VERSION-macos.sym
