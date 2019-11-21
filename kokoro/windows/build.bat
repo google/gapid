02ec9e861a2ae7c92e58d531f3eae4ee5ce8193d
John Plate <jplate@google.com>
2019-11-21 09:05:54 -0800

    hopefully final fix

--- kokoro/windows/build.bat
+++ kokoro/windows/build.bat
@@ -24,9 +24,8 @@ set JAVA_HOME=c:\Program Files\Java\jdk1.8.0_144
 
 REM Install the Android SDK components and NDK.
 set ANDROID_HOME=%LOCALAPPDATA%\Android\Sdk
-ren %ANDROID_HOME%\build-tools\26.0.0-preview 26.0.0-rc1
 copy /Y "%SRC%\kokoro\windows\android-sdk-license" "%ANDROID_HOME%\licenses\"
-echo y | %ANDROID_HOME%\tools\bin\sdkmanager platforms;android-26 build-tools;29.0.2
+call %ANDROID_HOME%\tools\bin\sdkmanager.bat platforms;android-26 build-tools;29.0.2
 wget -q https://dl.google.com/android/repository/android-ndk-r20b-windows-x86_64.zip
 unzip -q android-ndk-r20b-windows-x86_64.zip
 set ANDROID_NDK_HOME=%CD%\android-ndk-r20b
