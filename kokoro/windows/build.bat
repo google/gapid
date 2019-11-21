b72bc26aa89b09a0c10007c065d725c67dd40d2c
John Plate <jplate@google.com>
2019-11-20 18:58:52 -0800

    try to fix automatic Android license agreement

--- kokoro/windows/build.bat
+++ kokoro/windows/build.bat
@@ -24,7 +24,8 @@ set JAVA_HOME=c:\Program Files\Java\jdk1.8.0_144
 
 REM Install the Android SDK components and NDK.
 set ANDROID_HOME=%LOCALAPPDATA%\Android\Sdk
-echo y | %ANDROID_HOME%\tools\bin\sdkmanager build-tools;29.0.2 platforms;android-26
+(echo. & echo 24333f8a63b6825ea9c5514f83c2829b004d1fee) > "%ANDROID_HOME%\licenses\android-sdk-license"
+%ANDROID_HOME%\tools\bin\sdkmanager build-tools;29.0.2 platforms;android-26
 wget -q https://dl.google.com/android/repository/android-ndk-r20b-windows-x86_64.zip
 unzip -q android-ndk-r20b-windows-x86_64.zip
 set ANDROID_NDK_HOME=%CD%\android-ndk-r20b
