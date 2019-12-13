58260e674089e86f64dc925b90bb68a8fd833d4e
Hugues Evrard <hevrard@users.noreply.github.com>
2019-12-13 10:17:36 +0000

    Make validate_gpu_profiling return non-zero on validation failure (#3538)

--- cmd/gapit/validate_gpu_profiling.go
+++ cmd/gapit/validate_gpu_profiling.go
@@ -16,6 +16,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"os"
@@ -47,14 +48,19 @@ func (verb *validateGpuProfilingVerb) Run(ctx context.Context, flags flag.FlagSe
 		return log.Err(ctx, err, "Failed to get device list.")
 	}
 	stdout := os.Stdout
+	someDeviceFailed := false
 	for i, p := range devices {
 		fmt.Fprintf(stdout, "-- Device %v: %v --\n", i, p.ID.ID())
 		err = client.ValidateDevice(ctx, p)
 		if err != nil {
 			fmt.Fprintf(stdout, "%v\n", log.Err(ctx, err, "Failed to validate device"))
+			someDeviceFailed = true
 			continue
 		}
 		fmt.Fprintf(stdout, "Device is validated.\n")
+	}
+	if someDeviceFailed {
+		return errors.New("Some device failed validation")
 	}
 	return nil
 }
