a8755d2ee8379c5c203bd614db6acbd24d935a24
Pascal Muetschard <pmuetschard@google.com>
2019-12-20 13:36:15 -0800

    Use path.Pipelines to only fetch the bound pipelines in the UI.

--- gapic/src/main/com/google/gapid/util/Paths.java
+++ gapic/src/main/com/google/gapid/util/Paths.java
@@ -232,6 +232,16 @@ public class Paths {
         .build();
   }
 
+  public static Path.Any pipelinesAfter(CommandIndex command) {
+    if (command == null) {
+      return null;
+    }
+    return Path.Any.newBuilder()
+        .setPipelines(Path.Pipelines.newBuilder()
+            .setAfter(command.getCommand()))
+        .build();
+  }
+
   public static final Path.MeshOptions NODATA_MESH_OPTIONS = Path.MeshOptions.newBuilder()
       .setExcludeData(true)
       .build();
