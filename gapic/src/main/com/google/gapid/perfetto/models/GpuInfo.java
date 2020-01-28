abe12ae268540ecb90ed09a57170ded0e040a75f
rschiu <chiur@google.com>
2020-01-27 18:52:02 -0800

    Render vk_queue_submit event
    Change GPU Queue to use name from the gpu_track table.

--- gapic/src/main/com/google/gapid/perfetto/models/GpuInfo.java
+++ gapic/src/main/com/google/gapid/perfetto/models/GpuInfo.java
@@ -76,6 +76,7 @@ public class GpuInfo {
       res.forEachRow(($, r) -> {
         switch (r.getString(2)) {
           case "gpu_render_stage":
+          case "vulkan_events":
             queues.add(new Queue(queues.size(), r));
             break;
           case "graphics_frame_event":
@@ -93,20 +94,22 @@ public class GpuInfo {
   public static class Queue {
     public final int id;
     public final long trackId;
+    public final String name;
     public final int maxDepth;
 
-    public Queue(int id, long trackId, int maxDepth) {
+    public Queue(int id, long trackId, String name, int maxDepth) {
       this.id = id;
       this.trackId = trackId;
+      this.name = name;
       this.maxDepth = maxDepth;
     }
 
     public Queue(int id, QueryEngine.Row row) {
-      this(id, row.getLong(0), row.getInt(3));
+      this(id, row.getLong(0), row.getString(1), row.getInt(3));
     }
 
     public String getDisplay() {
-      return "GPU Queue " + id;
+      return name;
     }
   }
 
