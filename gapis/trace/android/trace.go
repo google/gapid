4e125331a99ae50288c9f26636b892b081473217
Pascal Muetschard <pmuetschard@google.com>
2020-02-10 07:29:29 -0800

    Disable taking a GLES trace.

--- gapis/trace/android/trace.go
+++ gapis/trace/android/trace.go
@@ -268,9 +268,11 @@ func NewTracer(dev bind.Device) tracer.Tracer {
 // TraceConfiguration returns the device's supported trace configuration.
 func (t *androidTracer) TraceConfiguration(ctx context.Context) (*service.DeviceTraceConfiguration, error) {
 	apis := make([]*service.TraceTypeCapabilities, 0, 3)
+	/* Disabled in AGI. Use GAPID instead.
 	if t.b.Instance().GetConfiguration().GetDrivers().GetOpengl().GetVersion() != "" {
 		apis = append(apis, tracer.GLESTraceOptions())
 	}
+	*/
 	if len(t.b.Instance().GetConfiguration().GetDrivers().GetVulkan().GetPhysicalDevices()) > 0 {
 		apis = append(apis, tracer.VulkanTraceOptions())
 	}
