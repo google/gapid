f0b261014c7bfc454f21c49b90e0d97f64e6816e
Pascal Muetschard <pmuetschard@google.com>
2020-03-25 14:27:58 -0700

    Depend on the libwebkitgtk package.
    This package contains the WebKit GTK library required by SWT to show
    an inline browser widget.

--- kokoro/linux/package.sh
+++ kokoro/linux/package.sh
@@ -58,7 +58,7 @@ Version: $VERSION
 Section: development
 Priority: optional
 Architecture: amd64
-Depends: openjdk-8-jre
+Depends: openjdk-8-jre, libwebkitgtk-1.0-0
 Maintainer: Google, Inc <gapid-team@google.com>
 Description: Android Graphics Inspector is a collection of tools that allows you
  to inspect, tweak and replay calls from an application to a graphics driver.
