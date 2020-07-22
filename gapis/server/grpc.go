82d0cd6f2fdd8e8343346f920750b1ed0a3bd31e
John Plate <jplate@google.com>
2020-07-22 18:32:10 +0100

    add mutex to grpcServer.interrupters (#405)
    add mutex to grpcServer.interrupters

--- gapis/server/grpc.go
+++ gapis/server/grpc.go
@@ -85,6 +85,8 @@ func NewWithListener(ctx context.Context, l net.Listener, cfg Config, srvChan ch
 	case err := <-done:
 		return err
 	case <-task.ShouldStop(ctx):
+		s.mutex.Lock()
+		defer s.mutex.Unlock()
 		for _, f := range s.interrupters {
 			f()
 		}
@@ -99,6 +101,7 @@ type grpcServer struct {
 	inFlightRPCs    int64
 	interrupters    map[int]func()
 	lastInterrupter int
+	mutex           sync.Mutex
 }
 
 // inRPC should be called at the start of an RPC call. The returned function
@@ -179,10 +182,16 @@ func (s *grpcServer) stopOnInterrupt(ctx context.Context, server *grpc.Server, s
 }
 
 func (s *grpcServer) addInterrupter(f func()) (remove func()) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	li := s.lastInterrupter
 	s.lastInterrupter++
 	s.interrupters[li] = f
-	return func() { delete(s.interrupters, li) }
+	return func() {
+		s.mutex.Lock()
+		defer s.mutex.Unlock()
+		delete(s.interrupters, li)
+	}
 }
 
 func (s *grpcServer) Ping(ctx xctx.Context, req *service.PingRequest) (*service.PingResponse, error) {
