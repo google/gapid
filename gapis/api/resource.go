2632d5430e4612a2c8b7f95991faa8c0ecd820f2
Pascal Muetschard <pmuetschard@google.com>
2021-09-22 14:42:58 -0700

    Get rid of program resources.
    Programs are a GLES thing and have no equivalent in Vulkan.

--- gapis/api/resource.go
+++ gapis/api/resource.go
@@ -100,8 +100,6 @@ func NewResourceData(data interface{}) *ResourceData {
 		return &ResourceData{Data: &ResourceData_Texture{data}}
 	case *Shader:
 		return &ResourceData{Data: &ResourceData_Shader{data}}
-	case *Program:
-		return &ResourceData{Data: &ResourceData_Program{data}}
 	case *Pipeline:
 		return &ResourceData{Data: &ResourceData_Pipeline{data}}
 	default:
